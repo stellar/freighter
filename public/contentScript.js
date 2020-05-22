bceca9510e339a72f74ce2681dedaa75e53ea52b
Piyal Basu <pbasu235@gmail.com>
2020-05-22 16:21:12 -0400

    content script update

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,7 +1,7 @@
 const url = chrome.extension.getURL("lyraApi.min.js");
 const s = document.createElement("script");
 s.src = url;
-(document.head || document.documentElement).appendChild(s);
+(document.head || document.documentElement).prepend(s);
 s.onload = function() {
   s.parentNode.removeChild(s);
 };
