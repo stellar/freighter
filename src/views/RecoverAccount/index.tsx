21a65cecef92f14cc37e6914cdda3d37a52c41d8
Piyal Basu <pbasu235@gmail.com>
2020-05-22 12:44:54 -0400

    Piyal dev 2 (#5)
    * open ALL onboarding screens in a tab
    
    * some better naming
    
    * add comment

--- src/views/RecoverAccount/index.tsx
+++ src/views/RecoverAccount/index.tsx
@@ -96,7 +96,7 @@ const RecoverAccount = () => {
         Recover
       </button>
       <div>
-        <Link to="/">Go back</Link>
+        <Link to="/?sameTab=true">Go back</Link>
       </div>
       <ErrorMessage authError={authError}></ErrorMessage>
     </form>
