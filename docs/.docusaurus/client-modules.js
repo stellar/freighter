188279ce9d993d346424c1bbc7b95b7ad20ca99e
Piyal Basu <pbasu235@gmail.com>
2020-07-27 19:06:59 -0400

    remove some boilerplate

--- docs/.docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("/Users/piyalbasu/Stellar/lyra/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/piyalbasu/Stellar/lyra/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/piyalbasu/Stellar/lyra/node_modules/bootstrap/dist/css/bootstrap.min.css"),
-];
