31dd43cd383bedf24142c8f8e9ce8b1a1404a0b6
Piyal Basu <pbasu235@gmail.com>
2020-07-27 18:43:07 -0400

    fix playground and api paths

--- extension/public/playground/js/index.js
+++ extension/public/playground/js/index.js
@@ -1,57 +1,53 @@
-// import {
-//   getPublicKey,
-//   signTransaction,
-//   isConnected,
-// } from "../../../src/contentScript/lyraApi";
+import { getPublicKey, signTransaction, isConnected } from "@stellar/lyra-api";
 
-// document
-//   .querySelector("#lyra-isConnected")
-//   .addEventListener("click", async () => {
-//     let response = false;
+document
+  .querySelector("#lyra-isConnected")
+  .addEventListener("click", async () => {
+    let response = false;
 
-//     try {
-//       response = await isConnected();
-//     } catch (e) {
-//       console.error(e);
-//     }
+    try {
+      response = await isConnected();
+    } catch (e) {
+      console.error(e);
+    }
 
-//     document.querySelector("#isConnected").value = response;
-//   });
+    document.querySelector("#isConnected").value = response;
+  });
 
-// document.querySelector("#lyra-connect").addEventListener("click", async () => {
-//   let response = "";
+document.querySelector("#lyra-connect").addEventListener("click", async () => {
+  let response = "";
 
-//   try {
-//     response = await getPublicKey();
-//   } catch (e) {
-//     console.error(e);
-//   }
-//   const { publicKey, error } = response;
+  try {
+    response = await getPublicKey();
+  } catch (e) {
+    console.error(e);
+  }
+  const { publicKey, error } = response;
 
-//   const output = publicKey || error;
+  const output = publicKey || error;
 
-//   document.querySelector("#publicKey").value = output;
-// });
+  document.querySelector("#publicKey").value = output;
+});
 
-// document
-//   .querySelector("#lyra-submit-transaction")
-//   .addEventListener("click", async () => {
-//     const transactionXdr = document.querySelector("#transaction-xdr").value;
+document
+  .querySelector("#lyra-submit-transaction")
+  .addEventListener("click", async () => {
+    const transactionXdr = document.querySelector("#transaction-xdr").value;
 
-//     let response = "";
+    let response = "";
 
-//     try {
-//       response = await signTransaction({
-//         transactionXdr,
-//       });
-//     } catch (e) {
-//       console.error(e);
-//     }
+    try {
+      response = await signTransaction({
+        transactionXdr,
+      });
+    } catch (e) {
+      console.error(e);
+    }
 
-//     const { transactionStatus, error } = response;
+    const { transactionStatus, error } = response;
 
-//     const output = transactionStatus || error;
-//     document.querySelector("#transaction-status").value = JSON.stringify(
-//       output,
-//     );
-//   });
+    const output = transactionStatus || error;
+    document.querySelector("#transaction-status").value = JSON.stringify(
+      output,
+    );
+  });
