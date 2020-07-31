356ab81c3b8a6aaa676470e8af517fe44c2ee091
Carl Vitullo <carl@stellar.org>
2020-07-31 13:59:23 -0400

    Add more visible metric

--- extension/src/popup/metrics/authServices.ts
+++ extension/src/popup/metrics/authServices.ts
@@ -2,9 +2,12 @@ import { METRIC_NAMES } from "popup/constants/metricsNames";
 
 import { registerHandler, emitMetric } from "helpers/metrics";
 
-import { createAccount } from "popup/ducks/authServices";
+import { createAccount, confirmPassword } from "popup/ducks/authServices";
 import { AppState } from "popup/App";
 
 registerHandler<AppState>(createAccount.fulfilled, () => {
   emitMetric(METRIC_NAMES.newWallet);
+});
+registerHandler<AppState>(confirmPassword.fulfilled, () => {
+  emitMetric(METRIC_NAMES.confirmPassword);
 });
