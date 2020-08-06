44f020636c2d3c7676762ea58279df6659fe6909
Carl Vitullo <carl@stellar.org>
2020-08-06 19:14:51 -0400

    Add view metrics

---
+++ extension/src/popup/constants/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  home: "/",
+  account: "/account",
+  signTransaction: "/sign-transaction",
+  grantAccess: "/grant-access",
+  mnemonicPhrase: "/mnemonic-phrase",
+  unlockAccount: "/unlock-account",
+  mnemonicPhraseConfirmed: "/mnemonic-phrase-confirmed",
+  createPassword: "/create-password",
+  recoverAccount: "/recover-account",
+};
---
+++ extension/src/popup/metrics/views.ts
@@ -0,0 +1,30 @@
+import { METRIC_NAMES } from "popup/constants/metricsNames";
+
+import { registerHandler, emitMetric } from "helpers/metrics";
+
+import { navigate } from "popup/ducks/views";
+import { AppState } from "popup/App";
+import { ROUTES } from "popup/constants/routes";
+
+const routeToEventName = {
+  [ROUTES.home]: METRIC_NAMES.viewHome,
+  [ROUTES.account]: METRIC_NAMES.viewAccount,
+  [ROUTES.signTransaction]: METRIC_NAMES.viewSignTransaction,
+  [ROUTES.grantAccess]: METRIC_NAMES.viewGrantAccess,
+  [ROUTES.mnemonicPhrase]: METRIC_NAMES.viewMnemonicPhrase,
+  [ROUTES.unlockAccount]: METRIC_NAMES.viewUnlockAccount,
+  [ROUTES.mnemonicPhraseConfirmed]: METRIC_NAMES.viewMnemonicPhraseConfirmed,
+  [ROUTES.createPassword]: METRIC_NAMES.viewCreatePassword,
+  [ROUTES.recoverAccount]: METRIC_NAMES.viewRecoverAccount,
+};
+
+registerHandler<AppState>(navigate, (_, a) => {
+  // Awkward, but gives us types on action payload
+  const action = a as ReturnType<typeof navigate>;
+  const { pathname } = action.payload.location;
+  const eventName = routeToEventName[pathname];
+  if (!eventName) {
+    throw new Error(`Didn't find a metric event name for path '${pathname}'`);
+  }
+  emitMetric(eventName);
+});
