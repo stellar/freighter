b99c9d82b96d59d3000c01b4d4858a03e9c9b3b1
Piyal Basu <pbasu235@gmail.com>
2020-08-11 15:28:29 -0400

    remove unnecessary permissions from manifest (#64)

--- extension/public/background.ts
+++ extension/public/background.ts
@@ -1,20 +1,5 @@
 import { initMessageListener } from "../src/background";
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            css: ["div"],
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
-});
-
 chrome.runtime.onMessage.addListener((message) => {
   if (message === "runContentScript") {
     chrome.tabs.executeScript({
