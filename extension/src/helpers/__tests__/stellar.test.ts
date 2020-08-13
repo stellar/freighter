5f712ec4eccf3968e400490b29880c9eb5f54339
Carl Vitullo <carl@stellar.org>
2020-08-13 14:48:56 -0400

    Add test

---
+++ extension/src/helpers/__tests__/stellar.test.ts
@@ -0,0 +1,11 @@
+import { truncatedPublicKey } from "../stellar";
+
+describe("truncatedPublicKey", () => {
+  it("truncates keys", () => {
+    expect(
+      truncatedPublicKey(
+        "GAJVUHQV535IYW25XBTWTCUXNHLQN4F2PGIPOOX4DDKL2UPNXUHWU7B3",
+      ),
+    ).toBe("GAJV…U7B3");
+  });
+});
