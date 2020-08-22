ff901af558436b3fe9247060c2086889bb9ab644
Carl Vitullo <carl@stellar.org>
2020-08-22 00:38:16 -0400

    Open welcome tab on install

--- extension/public/background.ts
+++ extension/public/background.ts
@@ -1,11 +1,27 @@
-import { initMessageListener } from "../src/background";
+import { initMessageListener } from "background";
+import { browser } from "webextension-polyfill-ts";
+import { ROUTES } from "popup/constants/routes";
 
-chrome.runtime.onMessage.addListener((message) => {
+browser.runtime.onMessage.addListener((message) => {
   if (message === "runContentScript") {
-    chrome.tabs.executeScript({
+    browser.tabs.executeScript({
       file: "contentScript.min.js",
     });
   }
 });
 
 initMessageListener();
+
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+  if (temporary) return; // skip during development
+  switch (reason) {
+    case "install":
+      await browser.tabs.create({
+        url: browser.runtime.getURL(`index.html#${ROUTES.welcome}`),
+      });
+      break;
+    // TODO: case "update":
+    // TODO: case "browser_update":
+    default:
+  }
+});
