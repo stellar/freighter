3793f43a11dbb94c14823497a04ad7fdf9c3215f
Piyal Basu <pbasu235@gmail.com>
2020-11-03 16:53:36 -0500

    Make sure FullscreenStyle overrides everything else

--- extension/src/popup/components/FullscreenStyle.ts
+++ extension/src/popup/components/FullscreenStyle.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const FullscreenStyle = createGlobalStyle`
   body, html {
-    height: 100%;
-    width: 100%;
+    height: 100% !important;
+    width: 100% !important;
   }
 `;
