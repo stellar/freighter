547ee93ada04ba7f342f0a3d990ebca99bc768af
Piyal Basu <pbasu235@gmail.com>
2020-12-09 14:57:08 -0500

    rename memo var

--- extension/src/popup/helpers/decodeMemo.ts
+++ extension/src/popup/helpers/decodeMemo.ts
@@ -3,14 +3,14 @@ import { get } from "lodash";
 
 import { MEMO_TYPES } from "popup/constants/memoTypes";
 
-export const decodeMemo = (_memo: {}) => {
-  const memoType = get(_memo, "_switch.name", "");
+export const decodeMemo = (memo: {}) => {
+  const memoType = get(memo, "_switch.name", "");
 
   if (memoType === MEMO_TYPES.MEMO_ID) {
-    return get(_memo, "_value.low", "");
+    return get(memo, "_value.low", "");
   }
   const decodeMethod = memoType === MEMO_TYPES.MEMO_HASH ? "hex" : "utf-8";
-  return buffer.Buffer.from(get(_memo, "_value.data", "")).toString(
+  return buffer.Buffer.from(get(memo, "_value.data", "")).toString(
     decodeMethod,
   );
 };
