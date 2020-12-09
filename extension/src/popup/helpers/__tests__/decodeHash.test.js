f9327e8f2bb2627d47f09a86e4f35b10e97f54fe
Piyal Basu <pbasu235@gmail.com>
2020-12-09 16:45:56 -0500

    Different memos types need to be decoded differently (#159)
    * Different memos types need to be decoded differently
    
    * rename memo var

---
+++ extension/src/popup/helpers/__tests__/decodeHash.test.js
@@ -0,0 +1,41 @@
+import { MEMO_TYPES } from "popup/constants/memoTypes";
+import { decodeMemo } from "../decodeMemo";
+
+describe("decodeMemo", () => {
+  it("should return a memo id", () => {
+    const MEMO = {
+      _switch: {
+        name: MEMO_TYPES.MEMO_ID,
+      },
+      _value: {
+        low: 999,
+      },
+    };
+
+    expect(decodeMemo(MEMO)).toBe(999);
+  });
+  it("should decode and return a hashed memo", () => {
+    const MEMO = {
+      _switch: {
+        name: MEMO_TYPES.MEMO_HASH,
+      },
+      _value: {
+        data: [84, 104, 105],
+      },
+    };
+
+    expect(decodeMemo(MEMO)).toBe("546869");
+  });
+  it("should decode and return a memo", () => {
+    const MEMO = {
+      _switch: {
+        name: MEMO_TYPES.MEMO_TEXT,
+      },
+      _value: {
+        data: [97, 115, 100, 102],
+      },
+    };
+
+    expect(decodeMemo(MEMO)).toBe("asdf");
+  });
+});
