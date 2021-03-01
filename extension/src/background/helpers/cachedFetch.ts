a98142227a1cd11c986c93d8946ab96153fbf1ec
Piyal Basu <pbasu235@gmail.com>
2021-02-28 20:14:07 -0500

    handle errors from api request

--- extension/src/background/helpers/cachedFetch.ts
+++ extension/src/background/helpers/cachedFetch.ts
@@ -9,12 +9,23 @@ export const cachedFetch = async (url: string) => {
   const time = date.getTime();
   const sevenDaysAgo = time - 7 * 24 * 60 * 60 * 1000;
 
+  let directoryLookupJson = JSON.parse(
+    localStorage.getItem(CACHED_FETCH_ID) || "{}",
+  );
+
   if (cachedDate < sevenDaysAgo) {
-    const res = await fetch(url);
-    const directoryLookupJson = await res.json();
-    localStorage.setItem(CACHED_FETCH_ID, JSON.stringify(directoryLookupJson));
-    localStorage.setItem(CACHED_FETCH_DATE_ID, time.toString());
-    return directoryLookupJson;
+    try {
+      const res = await fetch(url);
+      directoryLookupJson = await res.json();
+
+      localStorage.setItem(
+        CACHED_FETCH_ID,
+        JSON.stringify(directoryLookupJson),
+      );
+      localStorage.setItem(CACHED_FETCH_DATE_ID, time.toString());
+    } catch (e) {
+      console.error(e);
+    }
   }
-  return JSON.parse(localStorage.getItem(CACHED_FETCH_ID) || "{}");
+  return directoryLookupJson;
 };
