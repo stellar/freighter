41ae074076285ad64601d3abd4ccd256431c41e9
Piyal Basu <pbasu235@gmail.com>
2022-01-24 14:13:32 -0500

    Feature/add sds input (#341)
    * adding css loading in webpack
    
    * add implementation pattern for SDS Input using Formik

--- extension/src/popup/index.tsx
+++ extension/src/popup/index.tsx
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
+// Import global CSS from Stellar Design System
+import "@stellar/design-system/build/styles.min.css";
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById("root"));
