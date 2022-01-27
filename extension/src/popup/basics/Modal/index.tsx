328beb836691b6ef5f13106796329487c319cf8c
Piyal Basu <pbasu235@gmail.com>
2022-01-27 10:56:58 -0500

    Reskin/Authentication Request

--- extension/src/popup/basics/Modal/index.tsx
+++ extension/src/popup/basics/Modal/index.tsx
@@ -2,10 +2,34 @@ import React from "react";
 
 import "./styles.scss";
 
+interface ModalHeaderProps {
+  children: React.ReactNode;
+}
+
+export const ModalHeader = ({ children }: ModalHeaderProps) => (
+  <section className="ModalWrapper--header">{children}</section>
+);
+
 interface ModalWrapperProps {
   children: React.ReactNode;
 }
 
 export const ModalWrapper = ({ children }: ModalWrapperProps) => (
   <section className="ModalWrapper">{children}</section>
+);
+
+interface ButtonsContainerProps {
+  children: React.ReactNode;
+}
+
+export const ButtonsContainer = ({ children }: ButtonsContainerProps) => (
+  <div className="ModalWrapper--buttons-container">{children}</div>
+);
+
+interface LowButtonContainerProps {
+  children: React.ReactNode;
+}
+
+export const LowButtonContainer = ({ children }: LowButtonContainerProps) => (
+  <div className="ModalWrapper--low-button-container">{children}</div>
 );
