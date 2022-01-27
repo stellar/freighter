07dbf685a0ffc7f453cd671d9a77060b14e9b2f4
Piyal Basu <pbasu235@gmail.com>
2022-01-27 17:10:26 -0500

    Reskin/Authentication Request (#356)
    * Reskin network mismatch
    
    * remove unneeded var
    
    * Reskin/Authentication Request
    
    * rename to singleButtonContainer

--- extension/src/popup/basics/Modal/index.tsx
+++ extension/src/popup/basics/Modal/index.tsx
@@ -2,10 +2,36 @@ import React from "react";
 
 import "./styles.scss";
 
+interface ModalHeaderProps {
+  children: React.ReactNode;
+}
+
+export const ModalHeader = ({ children }: ModalHeaderProps) => (
+  <section className="ModalWrapper--header">{children}</section>
+);
+
 interface ModalWrapperProps {
   children: React.ReactNode;
 }
 
 export const ModalWrapper = ({ children }: ModalWrapperProps) => (
   <section className="ModalWrapper">{children}</section>
+);
+
+interface ButtonsContainerProps {
+  children: React.ReactNode;
+}
+
+export const ButtonsContainer = ({ children }: ButtonsContainerProps) => (
+  <div className="ModalWrapper--buttons-container">{children}</div>
+);
+
+interface SingleButtonContainerProps {
+  children: React.ReactNode;
+}
+
+export const SingleButtonContainer = ({
+  children,
+}: SingleButtonContainerProps) => (
+  <div className="ModalWrapper--single-button-container">{children}</div>
 );
