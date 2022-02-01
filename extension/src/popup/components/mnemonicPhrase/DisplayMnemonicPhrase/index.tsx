502e68fc3af65659a008f4d49fe0ff8c352f6775
Alec Charbonneau <aleccharb21@gmail.com>
2022-02-01 15:16:37 -0800

    cleanup

--- extension/src/popup/components/mnemonicPhrase/DisplayMnemonicPhrase/index.tsx
+++ extension/src/popup/components/mnemonicPhrase/DisplayMnemonicPhrase/index.tsx
@@ -25,72 +25,69 @@ export const DisplayMnemonicPhrase = ({
 }: {
   mnemonicPhrase: string;
   setReadyToConfirm: (readyState: boolean) => void;
-}) => {
-  return (
-    <>
-      <FullscreenStyle />
-      <div className="DisplayMnemonicPhrase--screen">
-        <div className="DisplayMnemonicPhrase--half-screen">
-          <div className="DisplayMnemonicPhrase--header">
-            Secret Recovery phrase
-          </div>
-          <div className="DisplayMnemonicPhrase--content">
-            <p>
-              Your recovery phrase gives you access to your account and is the{" "}
-              <strong>only way to access it in a new browser</strong>. Keep it
-              in a safe place.
-            </p>
-            <p>
-              For your security, we'll check if you got it right in the next
-              step.
-            </p>
-          </div>
+}) => (
+  <>
+    <FullscreenStyle />
+    <div className="DisplayMnemonicPhrase--screen">
+      <div className="DisplayMnemonicPhrase--half-screen">
+        <div className="DisplayMnemonicPhrase--header">
+          Secret Recovery phrase
         </div>
-        <div className="DisplayMnemonicPhrase--half-screen">
-          <InfoBlock variant={InfoBlock.variant.warning}>
-            <strong>IMPORTANT WARNING</strong>
-            <p>Never disclose your recovery phrase!</p>
-          </InfoBlock>
-          <div className="DisplayMnemonicPhrase--mnemonic-display">
-            <Card variant={Card.variant.highlight}>
-              <ol className="DisplayMnemonicPhrase--ordered-list">
-                {mnemonicPhrase.split(" ").map((word: string) => (
-                  <li>
-                    <strong>{word}</strong>
-                  </li>
-                ))}
-              </ol>
-            </Card>
-          </div>
-          {/* TODO - use Custom Button from SDS when added */}
-          <div className="DisplayMnemonicPhrase--display-buttons">
-            <ActionButton
-              data-testid="download"
-              onClick={() => {
-                download({
-                  filename: "freighterMnemonicPhrase.txt",
-                  content: mnemonicPhrase,
-                });
-                emitMetric(METRIC_NAMES.accountCreatorMnemonicDownloadPhrase);
-              }}
-            >
-              <TextLink>DOWNLOAD</TextLink>
-              <img src={DownloadIcon} alt="download button" />
-            </ActionButton>
-            <CopyText textToCopy={mnemonicPhrase} showCopyIcon showTooltip>
-              <TextLink>COPY</TextLink>
-            </CopyText>
-          </div>
-          <Button
-            fullWidth
-            onClick={() => {
-              setReadyToConfirm(true);
-            }}
-          >
-            Next
-          </Button>
+        <div className="DisplayMnemonicPhrase--content">
+          <p>
+            Your recovery phrase gives you access to your account and is the{" "}
+            <strong>only way to access it in a new browser</strong>. Keep it in
+            a safe place.
+          </p>
+          <p>
+            For your security, we'll check if you got it right in the next step.
+          </p>
         </div>
       </div>
-    </>
-  );
-};
+      <div className="DisplayMnemonicPhrase--half-screen">
+        <InfoBlock variant={InfoBlock.variant.warning}>
+          <strong>IMPORTANT WARNING</strong>
+          <p>Never disclose your recovery phrase!</p>
+        </InfoBlock>
+        <div className="DisplayMnemonicPhrase--mnemonic-display">
+          <Card variant={Card.variant.highlight}>
+            <ol className="DisplayMnemonicPhrase--ordered-list">
+              {mnemonicPhrase.split(" ").map((word: string) => (
+                <li>
+                  <strong>{word}</strong>
+                </li>
+              ))}
+            </ol>
+          </Card>
+        </div>
+        {/* TODO - use Custom Button from SDS when added */}
+        <div className="DisplayMnemonicPhrase--display-buttons">
+          <ActionButton
+            data-testid="download"
+            onClick={() => {
+              download({
+                filename: "freighterMnemonicPhrase.txt",
+                content: mnemonicPhrase,
+              });
+              emitMetric(METRIC_NAMES.accountCreatorMnemonicDownloadPhrase);
+            }}
+          >
+            <TextLink>DOWNLOAD</TextLink>
+            <img src={DownloadIcon} alt="download button" />
+          </ActionButton>
+          <CopyText textToCopy={mnemonicPhrase} showCopyIcon showTooltip>
+            <TextLink>COPY</TextLink>
+          </CopyText>
+        </div>
+        <Button
+          fullWidth
+          onClick={() => {
+            setReadyToConfirm(true);
+          }}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  </>
+);
