cac97015fd30aaae8e5123e974949ea01e3ec665
Alec Charbonneau <aleccharb21@gmail.com>
2022-02-02 09:07:31 -0800

    Mnemonicphrase: reskin DisplayMnemonicPhrase (#358)
    * first cut with todos
    
    * use copyText and button from sds
    
    * use scss
    
    * cleanup
    
    * cleanup

--- extension/src/popup/views/MnemonicPhrase.tsx
+++ extension/src/popup/views/MnemonicPhrase.tsx
@@ -1,24 +1,10 @@
 import React, { useState } from "react";
 import shuffle from "lodash/shuffle";
-import styled from "styled-components";
 
-import { emitMetric } from "helpers/metrics";
 import { useMnemonicPhrase } from "popup/helpers/useMnemonicPhrase";
-
-import { METRIC_NAMES } from "popup/constants/metricsNames";
-
 import { Header } from "popup/components/Header";
-import { Onboarding } from "popup/components/Onboarding";
 import { ConfirmMnemonicPhrase } from "popup/components/mnemonicPhrase/ConfirmMnemonicPhrase";
 import { DisplayMnemonicPhrase } from "popup/components/mnemonicPhrase/DisplayMnemonicPhrase";
-
-import ImportWalletIllo from "popup/assets/illo-backup-phrase.svg";
-
-const IconImgEl = styled.img`
-  height: 7.5rem;
-`;
-
-const IconEl = () => <IconImgEl src={ImportWalletIllo} alt="Import wallet" />;
 
 export const MnemonicPhrase = () => {
   const [readyToConfirm, setReadyToConfirm] = useState(false);
@@ -30,24 +16,12 @@ export const MnemonicPhrase = () => {
       <>
         <Header />
         {readyToConfirm ? (
-          <Onboarding
-            header="Confirm your backup phrase"
-            icon={<IconEl />}
-            subheader="Select each word in the correct order to confirm you got them right"
-            goBack={() => {
-              setReadyToConfirm(false);
-              emitMetric(METRIC_NAMES.accountCreatorConfirmMnemonicBack);
-            }}
-          >
-            <ConfirmMnemonicPhrase words={shuffle(mnemonicPhrase.split(" "))} />
-          </Onboarding>
+          <ConfirmMnemonicPhrase words={shuffle(mnemonicPhrase.split(" "))} />
         ) : (
-          <Onboarding header="Backup phrase" icon={<IconEl />}>
-            <DisplayMnemonicPhrase
-              mnemonicPhrase={mnemonicPhrase}
-              setReadyToConfirm={setReadyToConfirm}
-            />
-          </Onboarding>
+          <DisplayMnemonicPhrase
+            mnemonicPhrase={mnemonicPhrase}
+            setReadyToConfirm={setReadyToConfirm}
+          />
         )}
       </>
     );
