360d936f417550fb762c1aa899da8d71dcdc0f63
Alec Charbonneau <aleccharb21@gmail.com>
2022-03-03 15:19:45 -0800

    use FormRows from basic/forms

--- extension/src/popup/views/AddAccount/ImportAccount/index.tsx
+++ extension/src/popup/views/AddAccount/ImportAccount/index.tsx
@@ -11,6 +11,7 @@ import { emitMetric } from "helpers/metrics";
 import { PopupWrapper } from "popup/basics/PopupWrapper";
 import { BottomNav } from "popup/components/BottomNav";
 import { importAccount, authErrorSelector } from "popup/ducks/accountServices";
+import { FormRows } from "popup/basics/Forms";
 
 import { Button, Checkbox, Input, InfoBlock } from "@stellar/design-system";
 
@@ -55,21 +56,23 @@ export const ImportAccount = () => {
     <>
       <PopupWrapper>
         <div className="ImportAccount">
-          <InfoBlock variant={InfoBlock.variant.warning}>
-            <div className="ImportAccount__warning-header">
-              read before importing your key
-            </div>
-            <div className="ImportAccount__warning-copy">
-              <p>
-                Freighter can't recover your imported secret key using your
-                backup phrase. Storing your secret key is your responsability.{" "}
-              </p>
-              <p>
-                Freighter will never ask for your secret key outside of the
-                extension.
-              </p>
-            </div>
-          </InfoBlock>
+          <div className="ImportAccount__warning-block">
+            <InfoBlock variant={InfoBlock.variant.warning}>
+              <div className="ImportAccount__warning-header">
+                read before importing your key
+              </div>
+              <div className="ImportAccount__warning-copy">
+                <p>
+                  Freighter can't recover your imported secret key using your
+                  backup phrase. Storing your secret key is your responsability.{" "}
+                </p>
+                <p>
+                  Freighter will never ask for your secret key outside of the
+                  extension.
+                </p>
+              </div>
+            </InfoBlock>
+          </div>
           <Formik
             initialValues={initialValues}
             onSubmit={handleSubmit}
@@ -77,7 +80,7 @@ export const ImportAccount = () => {
           >
             {({ dirty, isSubmitting, isValid }) => (
               <Form>
-                <div className="ImportAccount__form-row">
+                <FormRows>
                   <Field name="privateKey">
                     {({ field }: FieldProps) => (
                       <Input
@@ -90,8 +93,6 @@ export const ImportAccount = () => {
                       />
                     )}
                   </Field>
-                </div>
-                <div className="ImportAccount__form-row">
                   <Field name="password">
                     {({ field }: FieldProps) => (
                       <Input
@@ -104,8 +105,6 @@ export const ImportAccount = () => {
                       />
                     )}
                   </Field>
-                </div>
-                <div className="ImportAccount__form-row">
                   <Field name="authorization">
                     {({ field }: FieldProps) => (
                       <Checkbox
@@ -116,7 +115,7 @@ export const ImportAccount = () => {
                       />
                     )}
                   </Field>
-                </div>
+                </FormRows>
                 <div className="ImportAccount__btn-row">
                   <Button
                     variant={Button.variant.tertiary}
