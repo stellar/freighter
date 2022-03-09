4fe51d025f6b3c2ee1cd958f10b8fad3252d6b7b
Alec Charbonneau <aleccharb21@gmail.com>
2022-03-09 15:32:25 -0800

    PR comment changes

--- extension/src/popup/components/sendPayment/SendConfirm/index.tsx
+++ extension/src/popup/components/sendPayment/SendConfirm/index.tsx
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
-import StellarSdk, { Asset } from "stellar-sdk";
-import { Types } from "@stellar/wallet-sdk";
-
-import { navigateTo } from "popup/helpers/navigate";
-import { ROUTES } from "popup/constants/routes";
 import {
-  signFreighterTransaction,
-  submitFreighterTransaction,
   ActionStatus,
   transactionSubmissionSelector,
 } from "popup/ducks/internalTransaction";
-import { AppDispatch } from "popup/App";
-import { settingsNetworkDetailsSelector } from "popup/ducks/settings";
-
 import { PopupWrapper } from "popup/basics/PopupWrapper";
-import { BackButton } from "popup/basics/BackButton";
 
 import { SubmitFail, SubmitPending, SubmitSuccess } from "./SubmitResult";
+import { TransactionDetails } from "./TransactionDetails";
 
 import "../styles.scss";
 
@@ -37,99 +27,26 @@ export const SendConfirm = ({
   transactionFee: string;
   memo: string;
 }) => {
-  const dispatch: AppDispatch = useDispatch();
-  const networkDetails = useSelector(settingsNetworkDetailsSelector);
   const submission = useSelector(transactionSubmissionSelector);
-  const [isViewOnly, setIsViewOnly] = useState(false);
-
-  // handles signing and submitting
-  const handleSend = async () => {
-    const server = new StellarSdk.Server(networkDetails.networkUrl);
-
-    let horizonAsset: Asset;
-    if (asset === "native") {
-      horizonAsset = StellarSdk.Asset.native();
-    } else {
-      horizonAsset = new StellarSdk.Asset(
-        asset.split(":")[0],
-        asset.split(":")[1],
-      );
-    }
-
-    const transactionXDR = await server
-      .loadAccount(publicKey)
-      .then((sourceAccount: Types.Account) => {
-        const transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
-          fee: transactionFee,
-          networkPassphrase: networkDetails.networkPassphrase,
-        })
-          .addOperation(
-            StellarSdk.Operation.payment({
-              destination,
-              asset: horizonAsset,
-              amount,
-            }),
-          )
-          .addMemo(StellarSdk.Memo.text(memo))
-          .setTimeout(180)
-          .build();
-
-        return transaction.toXDR();
-      });
-
-    const res = await dispatch(
-      signFreighterTransaction({
-        transactionXDR,
-        network: networkDetails.networkPassphrase,
-      }),
-    );
-
-    if (signFreighterTransaction.fulfilled.match(res)) {
-      const signedXDR = StellarSdk.TransactionBuilder.fromXDR(
-        res.payload.signedTransaction,
-        networkDetails.networkPassphrase,
-      );
-
-      await dispatch(
-        submitFreighterTransaction({
-          signedXDR,
-          networkUrl: networkDetails.networkUrl,
-        }),
-      );
-    }
-  };
-
-  const TransactionDetails = () => (
-    <div className="SendConfirm">
-      {isViewOnly ? (
-        <button onClick={() => navigateTo(ROUTES.account)}>Close</button>
-      ) : (
-        <BackButton hasBackCopy />
-      )}
-      <div className="header">Send Confirm</div>
-      <div>amount: {amount}</div>
-      <div>asset: {asset}</div>
-      <div>destination: {destination}</div>
-      <div>transactionFee: {transactionFee}</div>
-      <div>memo: {memo}</div>
-      {isViewOnly ? (
-        <button>View on Stellar.expert</button>
-      ) : (
-        <>
-          <button>cancel</button>
-          <button onClick={handleSend}>send</button>
-        </>
-      )}
-    </div>
-  );
+  const [isSendComplete, setIsSendComplete] = useState(false);
 
   const render = () => {
-    if (isViewOnly) {
-      return <TransactionDetails />;
+    const transactionDetailsProps = {
+      publicKey,
+      amount,
+      asset,
+      destination,
+      transactionFee,
+      memo,
+      isSendComplete,
+    };
+
+    if (isSendComplete) {
+      return <TransactionDetails {...transactionDetailsProps} />;
     }
     switch (submission.status) {
       case ActionStatus.IDLE:
-        return <TransactionDetails />;
+        return <TransactionDetails {...transactionDetailsProps} />;
       case ActionStatus.PENDING:
         return <SubmitPending />;
       case ActionStatus.SUCCESS:
@@ -138,13 +55,13 @@ export const SendConfirm = ({
             amount={amount}
             asset={asset}
             destination={destination}
-            viewDetails={() => setIsViewOnly(true)}
+            viewDetails={() => setIsSendComplete(true)}
           />
         );
       case ActionStatus.ERROR:
         return <SubmitFail destination={destination} />;
       default:
-        return <TransactionDetails />;
+        return <TransactionDetails {...transactionDetailsProps} />;
     }
   };
 
