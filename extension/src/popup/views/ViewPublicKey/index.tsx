0dc4c197b35ff926a00fa5d4c9d6dfe56e7694e2
Piyal Basu <pbasu235@gmail.com>
2022-03-09 20:43:18 -0500

    use new back button for edit account name

--- extension/src/popup/views/ViewPublicKey/index.tsx
+++ extension/src/popup/views/ViewPublicKey/index.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import QrCode from "qrcode.react";
-import { Formik, Field, FieldProps, Form } from "formik";
+import { Formik, Field, FieldProps, Form, useFormikContext } from "formik";
 
 import { emitMetric } from "helpers/metrics";
 import { truncatedPublicKey } from "helpers/stellar";
 
-import { ROUTES } from "popup/constants/routes";
 import { METRIC_NAMES } from "popup/constants/metricsNames";
-import { navigateTo, openTab } from "popup/helpers/navigate";
-import { BackButton } from "popup/basics/Buttons";
+import { openTab } from "popup/helpers/navigate";
+import { BackButton } from "popup/basics/BackButton";
 import { PopupWrapper } from "popup/basics/PopupWrapper";
 import { BottomNav } from "popup/components/BottomNav";
 import {
@@ -35,6 +34,20 @@ export const ViewPublicKey = () => {
   const [isEditingName, setIsEditingName] = useState(false);
   const { network } = useSelector(settingsNetworkDetailsSelector);
 
+  const EditNameButton = () => {
+    const { submitForm } = useFormikContext();
+
+    return isEditingName ? (
+      <button onClick={() => submitForm()}>
+        <Icon.Check />
+      </button>
+    ) : (
+      <button onClick={() => setIsEditingName(true)}>
+        <Icon.Edit />
+      </button>
+    );
+  };
+
   const dispatch = useDispatch();
 
   interface FormValue {
@@ -55,81 +68,80 @@ export const ViewPublicKey = () => {
   };
 
   return (
-    <PopupWrapper>
-      <div className="ViewPublicKey">
-        {/* TODO - use better back button and format nicely */}
-        <BackButton isPopup onClick={() => navigateTo(ROUTES.account)} />
-        <div className="ViewPublicKey__header">
-          {isEditingName ? (
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-              <Form className="ViewPublicKey__form">
-                <div className="ViewPublicKey--account-name-div"></div>
-                <Field name="accountName">
-                  {({ field }: FieldProps) => (
-                    <Input
-                      autoComplete="off"
-                      id="accountName"
-                      placeholder={accountName}
-                      {...field}
-                    />
-                  )}
-                </Field>
-                <div className="ViewPublicKey--account-name-div">
-                  <button type="submit">
-                    <Icon.Check />
-                  </button>
-                </div>
-              </Form>
-            </Formik>
-          ) : (
-            <>
-              <div className="ViewPublicKey--account-name-div"></div>
-              <div className="ViewPublicKey__account-name-display">
-                {accountName}
-              </div>
-              <div className="ViewPublicKey--account-name-div">
-                <button onClick={() => setIsEditingName(true)}>
-                  <Icon.Edit />
-                </button>
-              </div>
-            </>
-          )}
-        </div>
+    <>
+      <PopupWrapper>
+        <div className="ViewPublicKey">
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <div className="ViewPublicKey__header">
+              <BackButton />
+              {isEditingName ? (
+                <>
+                  <div className="ViewPublicKey--account-name-div"></div>
 
-        <div className="ViewPublicKey__qr-code">
-          <QrCode
-            value={publicKey}
-            style={{
-              width: "10rem",
-              height: "10rem",
-            }}
-          />
+                  <Form className="ViewPublicKey__form">
+                    <Field name="accountName">
+                      {({ field }: FieldProps) => (
+                        <Input
+                          autoComplete="off"
+                          id="accountName"
+                          placeholder={accountName}
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                  </Form>
+                </>
+              ) : (
+                <>
+                  <div className="ViewPublicKey--account-name-div"></div>
+                  <div className="ViewPublicKey__account-name-display">
+                    {accountName}
+                  </div>
+                </>
+              )}
+              <div className="ViewPublicKey--account-name-div">
+                <EditNameButton />
+              </div>
+            </div>
+          </Formik>
+
+          <div className="ViewPublicKey__qr-code">
+            <QrCode
+              value={publicKey}
+              style={{
+                width: "10rem",
+                height: "10rem",
+              }}
+            />
+          </div>
+          <div className="ViewPublicKey__address-copy-label">
+            Wallet Address
+          </div>
+          <div className="ViewPublicKey__address-copy">
+            {truncatedPublicKey(publicKey)}
+          </div>
+          <div className="ViewPublicKey__copy-btn">
+            <CopyText textToCopy={publicKey} doneLabel="ADDRESS COPIED">
+              <Button variant={Button.variant.tertiary}>COPY</Button>
+            </CopyText>
+          </div>
+          <div className="ViewPublicKey__external-link">
+            <TextLink
+              variant={TextLink.variant.secondary}
+              iconRight={<Icon.ExternalLink />}
+              onClick={() => {
+                openTab(
+                  `https://stellar.expert/explorer/${network.toLowerCase()}/account/${publicKey}`,
+                );
+                emitMetric(METRIC_NAMES.viewPublicKeyClickedStellarExpert);
+              }}
+            >
+              VIEW ON STELLAR.EXPERT
+            </TextLink>
+          </div>
         </div>
-        <div className="ViewPublicKey__address-copy-label">Wallet Address</div>
-        <div className="ViewPublicKey__address-copy">
-          {truncatedPublicKey(publicKey)}
-        </div>
-        <div className="ViewPublicKey__copy-btn">
-          <CopyText textToCopy={publicKey} doneLabel="ADDRESS COPIED">
-            <Button variant={Button.variant.tertiary}>COPY</Button>
-          </CopyText>
-        </div>
-        <div className="ViewPublicKey__external-link">
-          <TextLink
-            variant={TextLink.variant.secondary}
-            iconRight={<Icon.ExternalLink />}
-            onClick={() => {
-              openTab(
-                `https://stellar.expert/explorer/${network.toLowerCase()}/account/${publicKey}`,
-              );
-              emitMetric(METRIC_NAMES.viewPublicKeyClickedStellarExpert);
-            }}
-          >
-            VIEW ON STELLAR.EXPERT
-          </TextLink>
-        </div>
-      </div>
+      </PopupWrapper>
       <BottomNav />
-    </PopupWrapper>
+    </>
   );
 };
