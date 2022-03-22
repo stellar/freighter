64fc61d96d9d29a9a74b24733b86811877d93ac1
Piyal Basu <pbasu235@gmail.com>
2022-03-21 21:31:14 -0400

    pre QA cleanup (#402)

--- extension/src/popup/components/account/AccountHeader/index.tsx
+++ extension/src/popup/components/account/AccountHeader/index.tsx
@@ -42,94 +42,92 @@ export const AccountHeader = ({
   }, [allAccounts, isDropdownOpen]);
 
   return (
-    <>
-      <div className="AccountHeader" ref={accountDropDownRef}>
-        <div
-          className="AccountHeader__icon-btn"
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        >
-          <AccountListIdenticon
-            active
-            accountName={currentAccountName}
-            publicKey={publicKey}
-          />
-        </div>
-        <div className="AccountHeader__network-wrapper">
-          <div
-            className={`AccountHeader__network-icon ${
-              isTestnet ? "testnet" : "mainnet"
-            }`}
-          />
-          <div className="AccountHeader__network-copy">
-            {isTestnet ? "TEST NET" : "MAIN NET"}
-          </div>
-        </div>
-        <ul ref={dropdownRef} className="AccountHeader__options-dropdown">
-          {allAccounts.map(
-            ({ publicKey: accountPublicKey, name: accountName, imported }) => {
-              const isSelected = publicKey === accountPublicKey;
-
-              return (
-                <li
-                  className="AccountHeader__account-list-item"
-                  key={`account-${accountName}`}
-                >
-                  <AccountListIdenticon
-                    displayKey
-                    accountName={accountName}
-                    active={isSelected}
-                    publicKey={accountPublicKey}
-                    setIsDropdownOpen={setIsDropdownOpen}
-                  />
-                  {imported ? <ImportedTagEl /> : null}
-                  <span className="AccountHeader--option-check">
-                    {isSelected ? <Icon.Check /> : null}
-                  </span>
-                </li>
-              );
-            },
-          )}
-          <hr className="AccountHeader--list-divider" />
-          <li className="AccountHeader__option-list-item">
-            <Link
-              className="AccountHeader__option-link"
-              to={{
-                pathname: ROUTES.addAccount,
-                state: {
-                  header: "Create a new Stellar address",
-                  cta: "Add address",
-                },
-              }}
-            >
-              <div className="AccountHeader__option-icon">
-                <Icon.PlusCircle />
-              </div>
-              <span className="AccountHeader__option-link-copy">
-                Create a new Stellar address
-              </span>
-            </Link>
-          </li>
-          <li className="AccountHeader__option-list-item">
-            <Link
-              className="AccountHeader__option-link"
-              to={ROUTES.importAccount}
-            >
-              <div className="AccountHeader__option-icon">
-                <Icon.Download />
-              </div>
-              <span className="AccountHeader__option-link-copy">
-                Import a Stellar secret key
-              </span>
-            </Link>
-          </li>
-        </ul>
-        <div
-          onClick={() => setIsDropdownOpen(false)}
-          className={`AccountHeader__dropdown-background ${
-            isDropdownOpen ? "activate" : null
-          }`}
-        ></div>
+    <div className="AccountHeader" ref={accountDropDownRef}>
+      <div
+        className="AccountHeader__icon-btn"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        <AccountListIdenticon
+          active
+          accountName={currentAccountName}
+          publicKey={publicKey}
+        />
       </div>
-    </>
+      <div className="AccountHeader__network-wrapper">
+        <div
+          className={`AccountHeader__network-icon ${
+            isTestnet ? "testnet" : "mainnet"
+          }`}
+        />
+        <div className="AccountHeader__network-copy">
+          {isTestnet ? "TEST NET" : "MAIN NET"}
+        </div>
+      </div>
+      <ul ref={dropdownRef} className="AccountHeader__options-dropdown">
+        {allAccounts.map(
+          ({ publicKey: accountPublicKey, name: accountName, imported }) => {
+            const isSelected = publicKey === accountPublicKey;
+
+            return (
+              <li
+                className="AccountHeader__account-list-item"
+                key={`account-${accountName}`}
+              >
+                <AccountListIdenticon
+                  displayKey
+                  accountName={accountName}
+                  active={isSelected}
+                  publicKey={accountPublicKey}
+                  setIsDropdownOpen={setIsDropdownOpen}
+                />
+                {imported ? <ImportedTagEl /> : null}
+                <span className="AccountHeader--option-check">
+                  {isSelected ? <Icon.Check /> : null}
+                </span>
+              </li>
+            );
+          },
+        )}
+        <hr className="AccountHeader__list-divider" />
+        <li className="AccountHeader__option-list-item">
+          <Link
+            className="AccountHeader__option-link"
+            to={{
+              pathname: ROUTES.addAccount,
+              state: {
+                header: "Create a new Stellar address",
+                cta: "Add address",
+              },
+            }}
+          >
+            <div className="AccountHeader__option-icon">
+              <Icon.PlusCircle />
+            </div>
+            <span className="AccountHeader__option-link-copy">
+              Create a new Stellar address
+            </span>
+          </Link>
+        </li>
+        <li className="AccountHeader__option-list-item">
+          <Link
+            className="AccountHeader__option-link"
+            to={ROUTES.importAccount}
+          >
+            <div className="AccountHeader__option-icon">
+              <Icon.Download />
+            </div>
+            <span className="AccountHeader__option-link-copy">
+              Import a Stellar secret key
+            </span>
+          </Link>
+        </li>
+      </ul>
+      <div
+        onClick={() => setIsDropdownOpen(false)}
+        className={`AccountHeader__dropdown-background ${
+          isDropdownOpen ? "activate" : null
+        }`}
+      ></div>
+    </div>
   );
 };
