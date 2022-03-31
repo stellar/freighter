092d8e3cc7faa95d21ec6171cd7a2375e3312159
Alec Charbonneau <aleccharb21@gmail.com>
2022-03-31 10:41:40 -0700

    adding path payments pt2 (#413)
    * add allowedSlippage view
    
    * adding conversion rate to sendConfirm
    
    * added todos
    
    * try and fix weird submit error
    
    * update stellar-sdk packages
    
    * cleanup
    
    * cleanup
    
    * submit error view, cleanup
    
    * cleanup
    
    * move op code to constant
    
    * use helper
    
    * use forEach

---
+++ extension/src/popup/components/sendPayment/SendSettings/Slippage/index.tsx
@@ -0,0 +1,152 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Field, Form, Formik, FieldProps } from "formik";
+
+import {
+  Button,
+  Input,
+  Icon,
+  TextLink,
+  Card,
+  DetailsTooltip,
+} from "@stellar/design-system";
+
+import {
+  transactionDataSelector,
+  saveAllowedSlippage,
+} from "popup/ducks/transactionSubmission";
+import { navigateTo } from "popup/helpers/navigate";
+import { ROUTES } from "popup/constants/routes";
+import { PopupWrapper } from "popup/basics/PopupWrapper";
+
+import "./styles.scss";
+
+const defaultSlippage = "1";
+
+export const SendSettingsSlippage = () => {
+  const dispatch = useDispatch();
+  const { allowedSlippage } = useSelector(transactionDataSelector);
+
+  let presetSlippage = "";
+  let customSlippage = "";
+  if (["1", "2", "3"].includes(allowedSlippage)) {
+    presetSlippage = allowedSlippage;
+  } else {
+    customSlippage = allowedSlippage;
+  }
+
+  return (
+    <PopupWrapper>
+      <div className="Slippage__top-btns">
+        <div
+          className="Slippage__top-btns__exit"
+          onClick={() => navigateTo(ROUTES.sendPaymentSettings)}
+        >
+          <Icon.X />
+        </div>
+        <DetailsTooltip
+          // TODO - add copy
+          details=""
+        >
+          <span></span>
+        </DetailsTooltip>
+      </div>
+      <div className="Slippage">
+        <div className="SendPayment__header">Allowed Slippage</div>
+        <Formik
+          initialValues={{ presetSlippage, customSlippage }}
+          onSubmit={(values) => {
+            dispatch(
+              saveAllowedSlippage(
+                values.customSlippage || values.presetSlippage,
+              ),
+            );
+            navigateTo(ROUTES.sendPaymentSettings);
+          }}
+        >
+          {({ setFieldValue, values }) => (
+            <Form>
+              <div className="Slippage__cards">
+                <label className="Slippage--radio-label">
+                  <Field
+                    className="Slippage--radio-field"
+                    name="presetSlippage"
+                    type="radio"
+                    value="1"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setFieldValue("presetSlippage", e.target.value);
+                      setFieldValue("customSlippage", "");
+                    }}
+                  />
+                  <Card>1%</Card>
+                </label>
+                <label className="Slippage--radio-label">
+                  <Field
+                    className="Slippage--radio-field"
+                    name="presetSlippage"
+                    type="radio"
+                    value="2"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setFieldValue("presetSlippage", e.target.value);
+                      setFieldValue("customSlippage", "");
+                    }}
+                  />
+                  <Card>2%</Card>
+                </label>
+                <label className="Slippage--radio-label">
+                  <Field
+                    className="Slippage--radio-field"
+                    name="presetSlippage"
+                    type="radio"
+                    value="3"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setFieldValue("presetSlippage", e.target.value);
+                      setFieldValue("customSlippage", "");
+                    }}
+                  />
+                  <Card>3%</Card>
+                </label>
+              </div>
+              <div className="Slippage__custom-input">
+                <Field name="customSlippage">
+                  {({ field }: FieldProps) => (
+                    <Input
+                      id="custom-input"
+                      placeholder="Custom %"
+                      type="number"
+                      {...field}
+                      onChange={(e) => {
+                        setFieldValue("customSlippage", e.target.value);
+                        setFieldValue("presetSlippage", "");
+                      }}
+                    />
+                  )}
+                </Field>
+              </div>
+              <TextLink
+                underline
+                variant={TextLink.variant.secondary}
+                onClick={() => {
+                  setFieldValue("presetSlippage", defaultSlippage);
+                  setFieldValue("customSlippage", "");
+                }}
+              >
+                Set default
+              </TextLink>
+              <div className="SendPayment__btn-continue">
+                <Button
+                  fullWidth
+                  disabled={!values.presetSlippage && !values.customSlippage}
+                  variant={Button.variant.tertiary}
+                  type="submit"
+                >
+                  Done
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </PopupWrapper>
+  );
+};
