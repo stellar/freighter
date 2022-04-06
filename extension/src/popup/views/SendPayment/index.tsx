26fe648f4cec59235e03a6a25ac7ab183d129d7f
Alec Charbonneau <aleccharb21@gmail.com>
2022-04-06 13:58:27 -0700

    adding new verify view w/ todos

--- extension/src/popup/views/SendPayment/index.tsx
+++ extension/src/popup/views/SendPayment/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-import { Switch, Redirect } from "react-router-dom";
+import {
+  Switch,
+  Redirect,
+  Route,
+  useLocation,
+  RouteProps,
+} from "react-router-dom";
 import { PrivateKeyRoute, PublicKeyRoute } from "popup/Router";
 import { ROUTES } from "popup/constants/routes";
 
@@ -11,6 +18,43 @@ import { SendSettings } from "popup/components/sendPayment/SendSettings";
 import { SendSettingsFee } from "popup/components/sendPayment/SendSettings/TransactionFee";
 import { SendSettingsSlippage } from "popup/components/sendPayment/SendSettings/Slippage";
 import { SendConfirm } from "popup/components/sendPayment/SendConfirm";
+
+// ALEC TODO - possibly remove
+import { APPLICATION_STATE } from "@shared/constants/applicationState";
+import { AppError } from "popup/components/AppError";
+import {
+  applicationStateSelector,
+  hasPrivateKeySelector,
+  authErrorSelector,
+} from "popup/ducks/accountServices";
+
+// ALEC TODO - change name
+export const PrivateKeyRouteConfirm = (props: RouteProps) => {
+  const location = useLocation();
+  const applicationState = useSelector(applicationStateSelector);
+  const hasPrivateKey = useSelector(hasPrivateKeySelector);
+  const error = useSelector(authErrorSelector);
+
+  // ALEC TODO - need to check these here?
+  if (applicationState === APPLICATION_STATE.APPLICATION_ERROR) {
+    return <AppError>{error}</AppError>;
+  }
+  if (applicationState === APPLICATION_STATE.APPLICATION_LOADING) {
+    return null;
+  }
+  if (!hasPrivateKey) {
+    return (
+      <Redirect
+        to={{
+          pathname: ROUTES.verifyAccount,
+          search: location.search,
+          state: { from: location },
+        }}
+      />
+    );
+  }
+  return <Route {...props} />;
+};
 
 export const SendPayment = () => (
   <Switch>
