018c263420c71f3bc2f1e64a2a1cb3f7247c47a3
Piyal Basu <pbasu235@gmail.com>
2022-04-18 16:24:29 -0400

    fix Send Max logic and available funds bug (#437)

---
+++ extension/src/popup/helpers/useNetworkFees.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import StellarSdk from "stellar-sdk";
+
+import { stroopToXlm } from "helpers/stellar";
+import { settingsNetworkDetailsSelector } from "popup/ducks/settings";
+
+enum NetworkCongestion {
+  LOW = "Low",
+  MEDIUM = "Medium",
+  HIGH = "High",
+}
+
+export const useNetworkFees = () => {
+  const { networkUrl } = useSelector(settingsNetworkDetailsSelector);
+  const [recommendedFee, setRecommendedFee] = useState("");
+  const [networkCongestion, setNetworkCongestion] = useState(
+    "" as NetworkCongestion,
+  );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const server = new StellarSdk.Server(networkUrl);
+        const {
+          fee_charged: feeCharged,
+          ledger_capacity_usage: ledgerCapacityUsage,
+        } = await server.feeStats();
+        setRecommendedFee(stroopToXlm(feeCharged.mode).toString());
+        if (ledgerCapacityUsage > 0.5 && ledgerCapacityUsage <= 0.75) {
+          setNetworkCongestion(NetworkCongestion.MEDIUM);
+        } else if (ledgerCapacityUsage > 0.75) {
+          setNetworkCongestion(NetworkCongestion.HIGH);
+        } else {
+          setNetworkCongestion(NetworkCongestion.LOW);
+        }
+      } catch (e) {
+        // use default values
+        console.error(e);
+      }
+    })();
+  }, [networkUrl]);
+
+  return { recommendedFee, networkCongestion };
+};
