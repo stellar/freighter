a9a0764cc88a7d30b3df88b4649174f8d057c530
Piyal Basu <pbasu235@gmail.com>
2022-05-03 15:48:43 -0400

    str change (#472)

--- extension/src/popup/App.tsx
+++ extension/src/popup/App.tsx
@@ -56,7 +56,7 @@ if (process.env.SENTRY_KEY) {
 export function App() {
   return (
     <Provider store={store}>
-      <span>tesssst</span>
+      <span>tessst</span>
       <Router />
     </Provider>
   );
