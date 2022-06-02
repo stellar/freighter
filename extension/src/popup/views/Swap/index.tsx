f9ba7be91ba1e2ca3e023b5742335fe7605ae668
Alec Charbonneau <aleccharb21@gmail.com>
2022-06-02 15:20:35 -0700

    edge case

--- extension/src/popup/views/Swap/index.tsx
+++ extension/src/popup/views/Swap/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Switch, Redirect } from "react-router-dom";
 
 import { PublicKeyRoute, VerifiedAccountRoute } from "popup/Router";
@@ -8,26 +9,50 @@ import { SendSettings } from "popup/components/sendPayment/SendSettings";
 import { SendSettingsFee } from "popup/components/sendPayment/SendSettings/TransactionFee";
 import { SendSettingsSlippage } from "popup/components/sendPayment/SendSettings/Slippage";
 import { SendConfirm } from "popup/components/sendPayment/SendConfirm";
+import {
+  getAccountBalances,
+  transactionSubmissionSelector,
+} from "popup/ducks/transactionSubmission";
+import { publicKeySelector } from "popup/ducks/accountServices";
+import { settingsNetworkDetailsSelector } from "popup/ducks/settings";
 
-export const Swap = () => (
-  <Switch>
-    <PublicKeyRoute exact path={ROUTES.swap}>
-      <Redirect to={ROUTES.swapAmount} />
-    </PublicKeyRoute>
-    <PublicKeyRoute exact path={ROUTES.swapAmount}>
-      <SendAmount previous={ROUTES.account} next={ROUTES.swapSettings} />
-    </PublicKeyRoute>
-    <PublicKeyRoute exact path={ROUTES.swapSettings}>
-      <SendSettings previous={ROUTES.swapAmount} next={ROUTES.swapConfirm} />
-    </PublicKeyRoute>
-    <PublicKeyRoute exact path={ROUTES.swapSettingsFee}>
-      <SendSettingsFee previous={ROUTES.swapSettings} />
-    </PublicKeyRoute>
-    <PublicKeyRoute exact path={ROUTES.swapSettingsSlippage}>
-      <SendSettingsSlippage previous={ROUTES.swapSettings} />
-    </PublicKeyRoute>
-    <VerifiedAccountRoute exact path={ROUTES.swapConfirm}>
-      <SendConfirm previous={ROUTES.swapSettings} />
-    </VerifiedAccountRoute>
-  </Switch>
-);
+export const Swap = () => {
+  const dispatch = useDispatch();
+  const { accountBalances } = useSelector(transactionSubmissionSelector);
+  const publicKey = useSelector(publicKeySelector);
+  const networkDetails = useSelector(settingsNetworkDetailsSelector);
+
+  useEffect(() => {
+    if (!accountBalances.balances) {
+      dispatch(
+        getAccountBalances({
+          publicKey,
+          networkDetails,
+        }),
+      );
+    }
+  }, [dispatch, publicKey, networkDetails, accountBalances]);
+
+  return (
+    <Switch>
+      <PublicKeyRoute exact path={ROUTES.swap}>
+        <Redirect to={ROUTES.swapAmount} />
+      </PublicKeyRoute>
+      <PublicKeyRoute exact path={ROUTES.swapAmount}>
+        <SendAmount previous={ROUTES.account} next={ROUTES.swapSettings} />
+      </PublicKeyRoute>
+      <PublicKeyRoute exact path={ROUTES.swapSettings}>
+        <SendSettings previous={ROUTES.swapAmount} next={ROUTES.swapConfirm} />
+      </PublicKeyRoute>
+      <PublicKeyRoute exact path={ROUTES.swapSettingsFee}>
+        <SendSettingsFee previous={ROUTES.swapSettings} />
+      </PublicKeyRoute>
+      <PublicKeyRoute exact path={ROUTES.swapSettingsSlippage}>
+        <SendSettingsSlippage previous={ROUTES.swapSettings} />
+      </PublicKeyRoute>
+      <VerifiedAccountRoute exact path={ROUTES.swapConfirm}>
+        <SendConfirm previous={ROUTES.swapSettings} />
+      </VerifiedAccountRoute>
+    </Switch>
+  );
+};
