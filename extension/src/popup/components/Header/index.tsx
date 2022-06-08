f87d711d068f8c0aec72e75a2a2f8f8959839616
Alec Charbonneau <aleccharb21@gmail.com>
2022-06-08 16:13:07 -0700

    remove network from onboarding screens

--- extension/src/popup/components/Header/index.tsx
+++ extension/src/popup/components/Header/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
-import { settingsNetworkDetailsSelector } from "popup/ducks/settings";
 
 import FreighterLogoLockup from "popup/assets/logo-lockup-freighter.svg";
 
@@ -11,25 +8,10 @@ interface HeaderProps {
   isPopupView?: boolean;
 }
 
-export const Header = ({ isPopupView = false }: HeaderProps) => {
-  const { isTestnet, networkName } = useSelector(
-    settingsNetworkDetailsSelector,
-  );
-  return (
-    <header className={`Header ${isPopupView ? "Header__popup" : ""}`}>
-      <div className={isPopupView ? "" : "Header__fullscreen"}>
-        <img alt="Freighter logo" src={FreighterLogoLockup} />
-        {isPopupView ? null : (
-          <div className="Header__network">
-            <div
-              className={`Header__network__icon ${
-                isTestnet ? "Header__network__icon--testnet" : ""
-              }`}
-            />
-            <div className="Header__network__name">{networkName}</div>
-          </div>
-        )}
-      </div>
-    </header>
-  );
-};
+export const Header = ({ isPopupView = false }: HeaderProps) => (
+  <header className={`Header ${isPopupView ? "Header__popup" : ""}`}>
+    <div className={isPopupView ? "" : "Header__fullscreen"}>
+      <img alt="Freighter logo" src={FreighterLogoLockup} />
+    </div>
+  </header>
+);
