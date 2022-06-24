d48bf2bf5a1361e76d430923d0f0a83283a0b3b5
Alec Charbonneau <aleccharb21@gmail.com>
2022-06-24 13:25:37 -0700

    cleanup

--- extension/src/popup/components/hardwareConnect/LedgerConnect/index.tsx
+++ extension/src/popup/components/hardwareConnect/LedgerConnect/index.tsx
@@ -3,8 +3,6 @@ import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 import LedgerApi from "@ledgerhq/hw-app-str";
 import { Icon, InfoBlock } from "@stellar/design-system";
 
-import { importHardwareWallet } from "@shared/api/internal";
-
 import { Button } from "popup/basics/buttons/Button";
 import { navigateTo } from "popup/helpers/navigate";
 import { ROUTES } from "popup/constants/routes";
@@ -46,16 +44,6 @@ export const LedgerConnect = ({ goBack }: { goBack?: () => void }) => {
 
   const handleConnect = async () => {
     setIsConnecting(true);
-
-    // ALEC TODO - remove hardcode
-    console.log("attempting import");
-    const pubKey = "GAARKRDCQQJOQYKBUY4ZI5NJQGEU3QRTCOXENJBMTUY5O7KXKULSWDEH";
-    const walletType = "LEDGER";
-    try {
-      await importHardwareWallet(pubKey, walletType);
-    } catch (e) {
-      console.log({ e });
-    }
 
     try {
       const transport = await TransportWebUSB.request();
