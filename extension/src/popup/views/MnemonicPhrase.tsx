15a5d9f9b14dab0aec8d47d0f0d8dd5f19519c3a
Alec Charbonneau <aleccharb21@gmail.com>
2022-07-29 11:10:41 -0700

    if already confirmed mnemonic dont re-view

--- extension/src/popup/views/MnemonicPhrase.tsx
+++ extension/src/popup/views/MnemonicPhrase.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import shuffle from "lodash/shuffle";
-import { Switch } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
+
+import { APPLICATION_STATE } from "@shared/constants/applicationState";
 
 import { PublicKeyRoute } from "popup/Router";
 import { ROUTES } from "popup/constants/routes";
@@ -10,9 +13,15 @@ import { Header } from "popup/components/Header";
 import { Onboarding } from "popup/components/Onboarding";
 import { ConfirmMnemonicPhrase } from "popup/components/mnemonicPhrase/ConfirmMnemonicPhrase";
 import { DisplayMnemonicPhrase } from "popup/components/mnemonicPhrase/DisplayMnemonicPhrase";
+import { applicationStateSelector } from "popup/ducks/accountServices";
 
 export const MnemonicPhrase = () => {
   const mnemonicPhrase = useMnemonicPhrase();
+  const applicationState = useSelector(applicationStateSelector);
+
+  if (applicationState === APPLICATION_STATE.MNEMONIC_PHRASE_CONFIRMED) {
+    return <Redirect to={ROUTES.pinExtension} />;
+  }
 
   if (mnemonicPhrase) {
     return (
