00511e590037b2e5a31f0f67ceb16d8b9a4d9182
Piyal Basu <pbasu235@gmail.com>
2022-08-21 16:18:21 -0400

    Add a custom network and switch to it

--- @shared/constants/stellar.ts
+++ @shared/constants/stellar.ts
@@ -1,5 +1,7 @@
+import StellarSdk from "stellar-sdk";
+
 export enum NETWORK_NAMES {
-  TESTNET = "Testnet",
+  TESTNET = "Test Net",
   PUBNET = "Main Net",
 }
 
@@ -7,3 +9,34 @@ export enum NETWORKS {
   PUBLIC = "PUBLIC",
   TESTNET = "TESTNET",
 }
+
+export enum NETWORK_URLS {
+  PUBLIC = "https://horizon.stellar.org",
+  TESTNET = "https://horizon-testnet.stellar.org",
+}
+
+export interface NetworkDetails {
+  network: string;
+  networkName: string;
+  networkUrl: string;
+  networkPassphrase: string;
+}
+
+export const MAINNET_NETWORK_DETAILS = {
+  network: NETWORKS.PUBLIC,
+  networkName: NETWORK_NAMES.PUBNET,
+  networkUrl: NETWORK_URLS.PUBLIC,
+  networkPassphrase: StellarSdk.Networks.PUBLIC,
+} as NetworkDetails;
+
+export const TESTNET_NETWORK_DETAILS = {
+  network: NETWORKS.TESTNET,
+  networkName: NETWORK_NAMES.TESTNET,
+  networkUrl: NETWORK_URLS.TESTNET,
+  networkPassphrase: StellarSdk.Networks.TESTNET,
+} as NetworkDetails;
+
+export const DEFAULT_NETWORKS: Array<NetworkDetails> = [
+  MAINNET_NETWORK_DETAILS,
+  TESTNET_NETWORK_DETAILS,
+];
