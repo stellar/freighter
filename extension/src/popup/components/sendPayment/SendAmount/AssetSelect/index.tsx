a6c56dcef3e75d023ab5f78d702011d15cf2fcf2
Alec Charbonneau <aleccharb21@gmail.com>
2022-09-23 11:45:48 -0700

    first cut

--- extension/src/popup/components/sendPayment/SendAmount/AssetSelect/index.tsx
+++ extension/src/popup/components/sendPayment/SendAmount/AssetSelect/index.tsx
@@ -12,6 +12,8 @@ import {
   AssetSelectType,
 } from "popup/ducks/transactionSubmission";
 import { useIsSwap } from "popup/helpers/useIsSwap";
+import { useIsScamAsset } from "popup/helpers/useIsScamAsset";
+import IconWarning from "popup/assets/icon-warning-red.svg";
 
 import "./styles.scss";
 
@@ -64,6 +66,7 @@ export function PathPayAssetSelect({
   const dispatch = useDispatch();
   const { assetIcons } = useSelector(transactionSubmissionSelector);
   const isSwap = useIsSwap();
+  const isScamAsset = useIsScamAsset(assetCode, issuerKey);
 
   const handleSelectAsset = () => {
     dispatch(
@@ -100,6 +103,10 @@ export function PathPayAssetSelect({
           <span className="AssetSelect__medium-copy">
             {truncateLongAssetCode(assetCode)}
           </span>{" "}
+          {/* ALEC TODO - abstract to own component? */}
+          <span className="AssetSelect__scam-asset">
+            {isScamAsset && <img src={IconWarning} alt="warning" />}
+          </span>
           <Icon.ChevronDown />
         </div>
         <div className="AssetSelect__content__right">
