1f4f1c90471119a7abb0a7be327a9e9af694f09e
Alec Charbonneau <aleccharb21@gmail.com>
2022-12-13 18:45:24 +1100

    return string from dataStorage always

--- extension/src/background/helpers/dataStorage.ts
+++ extension/src/background/helpers/dataStorage.ts
@@ -11,8 +11,12 @@ export const dataStorage = {
     // TODO: re-enable defaults by passing an object. The value of the key-value pair will be the default
 
     const storageResult = await browser.storage.local.get(key);
+    let val = storageResult[key];
+    if (val && typeof val !== "string") {
+      val = JSON.stringify(storageResult[key]);
+    }
 
-    return storageResult[key];
+    return val;
   },
   setItem: async (setItemParams: SetItemParams) => {
     await browser.storage.local.set(setItemParams);
