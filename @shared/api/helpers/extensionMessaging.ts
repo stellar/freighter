8215d77520e19392953bb3f8729012a3fd1c3a7c
Piyal Basu <pbasu235@gmail.com>
2022-12-19 18:01:50 -0500

    rm unneeded optional chaining operator

--- @shared/api/helpers/extensionMessaging.ts
+++ @shared/api/helpers/extensionMessaging.ts
@@ -40,7 +40,7 @@ export const sendMessageToBackground = async (msg: {}): Promise<Response> => {
     // treat this as an external call because we're making the call from the browser, not the popup
     res = await sendMessageToContentScript(msg);
   } else {
-    res = await browser?.runtime.sendMessage(msg);
+    res = await browser.runtime.sendMessage(msg);
   }
 
   return res;
