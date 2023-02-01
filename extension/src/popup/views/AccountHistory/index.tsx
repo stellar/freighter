0d585ad762d1375304d5a46e31cc59a8001d75d6
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-02-01 15:10:38 -0700

    fixes flash of screen on account tab, tweaks loaders height to not cover tabs

--- extension/src/popup/views/AccountHistory/index.tsx
+++ extension/src/popup/views/AccountHistory/index.tsx
@@ -125,53 +125,56 @@ export const AccountHistory = () => {
     <TransactionDetail {...detailViewProps} />
   ) : (
     <div className="AccountHistory">
-      {isLoading && (
+      {isLoading ? (
         <div className="AccountHistory__loader">
           <Loader size="2rem" />
         </div>
+      ) : (
+        <div className="AccountHistory__wrapper">
+          <header className="AccountHistory__header">
+            {t("Transactions")}
+          </header>
+          <div className="AccountHistory__selector">
+            {Object.values(SELECTOR_OPTIONS).map((option) => (
+              <div
+                key={option}
+                className={`AccountHistory__selector__item ${
+                  option === selectedSegment
+                    ? "AccountHistory__selector__item--active"
+                    : ""
+                }`}
+                onClick={() => setSelectedSegment(option)}
+              >
+                {t(option)}
+              </div>
+            ))}
+          </div>
+          <div className="AccountHistory__list">
+            {historySegments?.[SELECTOR_OPTIONS[selectedSegment]].length ? (
+              <HistoryList>
+                <>
+                  {historySegments![SELECTOR_OPTIONS[selectedSegment]].map(
+                    (operation: HistoryItemOperation) => (
+                      <HistoryItem
+                        key={operation.id}
+                        operation={operation}
+                        publicKey={publicKey}
+                        url={stellarExpertUrl}
+                        setDetailViewProps={setDetailViewProps}
+                        setIsDetailViewShowing={setIsDetailViewShowing}
+                      />
+                    ),
+                  )}
+                </>
+              </HistoryList>
+            ) : (
+              <div>
+                {isAccountHistoryLoading ? null : t("No transactions to show")}
+              </div>
+            )}
+          </div>
+        </div>
       )}
-      <div className="AccountHistory__wrapper">
-        <header className="AccountHistory__header">{t("Transactions")}</header>
-        <div className="AccountHistory__selector">
-          {Object.values(SELECTOR_OPTIONS).map((option) => (
-            <div
-              key={option}
-              className={`AccountHistory__selector__item ${
-                option === selectedSegment
-                  ? "AccountHistory__selector__item--active"
-                  : ""
-              }`}
-              onClick={() => setSelectedSegment(option)}
-            >
-              {t(option)}
-            </div>
-          ))}
-        </div>
-        <div className="AccountHistory__list">
-          {historySegments?.[SELECTOR_OPTIONS[selectedSegment]].length ? (
-            <HistoryList>
-              <>
-                {historySegments[SELECTOR_OPTIONS[selectedSegment]].map(
-                  (operation: HistoryItemOperation) => (
-                    <HistoryItem
-                      key={operation.id}
-                      operation={operation}
-                      publicKey={publicKey}
-                      url={stellarExpertUrl}
-                      setDetailViewProps={setDetailViewProps}
-                      setIsDetailViewShowing={setIsDetailViewShowing}
-                    />
-                  ),
-                )}
-              </>
-            </HistoryList>
-          ) : (
-            <div>
-              {isAccountHistoryLoading ? null : t("No transactions to show")}
-            </div>
-          )}
-        </div>
-      </div>
       <BottomNav />
     </div>
   );
