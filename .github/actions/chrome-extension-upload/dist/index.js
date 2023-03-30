df72360936b89e3cefb311259d630ef0f7954735
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-03-30 17:26:36 -0600

    adds updated compiled action

--- .github/actions/chrome-extension-upload/dist/index.js
+++ .github/actions/chrome-extension-upload/dist/index.js
@@ -17961,6 +17961,7 @@ async function run() {
     const filePath = core.getInput('file-path', {required: true})
     const extensionId = core.getInput('extension-id', {required: true})
     const clientId = core.getInput('client-id', {required: true})
+    const clientSecret = core.getInput('client-secret', {required: true})
     const refreshToken = core.getInput('refresh-token', {required: true})
     const globFlg = core.getInput('glob')
     const publishFlg = core.getInput('publish')
@@ -17969,6 +17970,7 @@ async function run() {
     const webStore = chromeWebstoreUpload({
       extensionId,
       clientId,
+      clientSecret,
       refreshToken
     })
 
