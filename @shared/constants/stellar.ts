f88a34abf13ae408c569136faeec03f8ff6b8b1a
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-04-13 09:36:00 -0600

    adds friendbotUrl input field for custom network

--- @shared/constants/stellar.ts
+++ @shared/constants/stellar.ts
@@ -24,7 +24,7 @@ export enum FRIENDBOT_URLS {
 }
 
 export const SOROBAN_RPC_URLS = {
-  futureNet: "https://rpc-futurenet.stellar.org/",
+  FUTURENET: "https://rpc-futurenet.stellar.org/",
 };
 
 export interface NetworkDetails {
