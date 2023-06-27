53caa83bea14646385eb5b94542338f4fdcb080a
Piyal Basu <pbasu235@gmail.com>
2023-06-27 12:38:30 -0400

    change job name

--- extension/src/popup/index.tsx
+++ extension/src/popup/index.tsx
@@ -6,7 +6,7 @@ import "popup/helpers/localizationConfig";
 // Import global CSS from Stellar Design System
 import "@stellar/design-system/build/styles.min.css";
 
-console.log("tesst");
+console.log("test");
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById("root"));
