960eafc4151ba7123fcd2ee43166ed9e8141fdce
Piyal Basu <pbasu235@gmail.com>
2023-06-29 20:47:30 -0400

    parens

--- .husky/addTranslations.sh
+++ .husky/addTranslations.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 yarn build:extension:translations
-git diff --quiet || echo "Adding trannslations commit. Please retry git push" &&  exit 1
+git diff --quiet || (echo "Adding trannslations commit. Please retry git push" &&  exit 1)
