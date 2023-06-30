1666601e2d815d4dcdceaafca063a722e0f74eff
Piyal Basu <pbasu235@gmail.com>
2023-06-29 20:40:54 -0400

    str

--- .husky/addTranslations.sh
+++ .husky/addTranslations.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 yarn build:extension:translations
-git diff || echo "hi" && echo "Adding translations commit. Please retry git push" && git add . && git commit -m "Added translations" && exit 1
+git diff || echo "hi" && echo "Adding trannslations commit. Please retry git push" && git add . && git commit -m "Added translations" && exit 1
