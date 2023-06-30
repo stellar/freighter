abaf34c8f3d122daef8cfe785fbe218daf85e149
Piyal Basu <pbasu235@gmail.com>
2023-06-29 20:45:47 -0400

    adjust script

--- .husky/addTranslations.sh
+++ .husky/addTranslations.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 yarn build:extension:translations
-git diff --quiet && git diff --staged --quiet || echo "hi" && echo "Adding trannslations commit. Please retry git push" && git add . && git commit -m "Added translations" && exit 1
+git diff --quiet || echo "Adding trannslations commit. Please retry git push" &&  exit 1
