d93eac835da46c1c37f0001fd5281305e676af28
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-07-31 14:48:05 -0600

    manually format changes

--- extension/src/popup/components/signBlob/index.tsx
+++ extension/src/popup/components/signBlob/index.tsx
@@ -4,14 +4,12 @@ import { Card, Heading4 } from "@stellar/design-system";
 import "./index.scss";
 
 interface BlobProps {
-  blob: string
+  blob: string;
 }
 
 export const Blob = (props: BlobProps) => (
   <Card variant={Card.variant.highlight}>
     <Heading4>Signing data:</Heading4>
-    <div className="Blob">
-      {props.blob}
-    </div>
+    <div className="Blob">{props.blob}</div>
   </Card>
-)
+);
