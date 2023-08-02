a15af6e92886f7a262200576b311c7ab570193db
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-08-02 10:19:12 -0600

    refactor upload script, uses archive correctly

--- .github/scripts/archive-safari-app.sh
+++ .github/scripts/archive-safari-app.sh
@@ -2,12 +2,15 @@
 
 set -eo pipefail
 
-xcodebuild -scheme Freighter -archivePath ./build/Freighter.xarchive build
+# convert extension for apple platforms
+xcrun safari-web-extension-converter ./build --project-location ./apple-build
 
-xcodebuild -archivePath ./build/Freighter.xarchive \
-            -exportOptionsPlist Freighter.plist \
-            -exportPath ./build \
+xcodebuild  -project ./apple-build/Freighter/Freighter.xcodeproj -scheme Freighter -archivePath /tmp/build-archive.xcarchive archive
+
+xcodebuild -archivePath /tmp/build-archive.xcarchive \
+            -exportOptionsPlist /tmp/build-archive.xcarchive/Info.plist  \
+            -exportPath ./final \
             -allowProvisioningUpdates \
-            -exportArchive | xcpretty
+            -exportArchive
 
-xcrun altool --upload-package --apple-id $APPLE_ID --asc-public-id $APPLE_PUBLIC_ID -t ios -t macos -f build/Freighter.ipa --apiKey $API_KEY --apiIssuer $API_ISSUER --verbose
+xcrun altool --upload-package --apple-id $APPLE_ID -t ios -t macos -f final/Freighter.ipa --apiKey $API_KEY --apiIssuer $API_ISSUER --verbose
