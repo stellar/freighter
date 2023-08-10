a2a1f3eedd0cd54ca18c178768136598f828a8bd
Piyal Basu <pbasu235@gmail.com>
2023-08-10 18:39:59 -0400

    define env vars using definePlugin

---
+++ extension/src/constants/env.ts
@@ -0,0 +1,6 @@
+declare const AMPLITUDE_KEY: string;
+const _AMPLITUDE_KEY = AMPLITUDE_KEY;
+
+declare const SENTRY_KEY: string;
+const _SENTRY_KEY = SENTRY_KEY;
+export { _AMPLITUDE_KEY as AMPLITUDE_KEY, _SENTRY_KEY as SENTRY_KEY };
--- extension/webpack.extension.js
+++ extension/webpack.extension.js
@@ -5,44 +5,57 @@ const { commonConfig } = require("./webpack.common.js");
 
 const LOCALES = ["en", "pt"];
 
-const prodConfig = (env = { PRODUCTION: false, TRANSLATIONS: false }) => ({
-  mode: "production",
-  optimization: {
-    minimize: env.PRODUCTION,
-    splitChunks: {
-      // ignore non-index.min.js chunk
-      chunks: (chunk) => chunk.name === "index",
-      // Firefox addon store has a max file size of 4mb
-      maxSize: 4000000,
-    },
+const prodConfig = (
+  env = {
+    PRODUCTION: false,
+    TRANSLATIONS: false,
+    AMPLITUDE_KEY: "",
+    SENTRY_KEY: "",
   },
+) => {
+  console.log(env);
 
-  plugins: [
-    new webpack.DefinePlugin({
-      DEV_SERVER: false,
-    }),
-    ...(env.TRANSLATIONS
-      ? [
-          new I18nextWebpackPlugin({
-            async: true,
-            dest: "./",
-            extensions: [".ts", ".tsx"],
-            options: {
-              createOldCatalogs: false,
-              locales: LOCALES,
-              output: "src/popup/locales/$LOCALE/$NAMESPACE.json",
-              sort: true,
-              useKeysAsDefaultValue: true,
-            },
-          }),
-        ]
-      : []),
-  ],
-  // This if to fine tune logged output. Since this is an extension, not a
-  // webapp, we don't face the same bundle size constraints of the web.
-  performance: {
-    hints: false,
-  },
-});
+  return {
+    mode: "production",
+    optimization: {
+      minimize: env.PRODUCTION,
+      splitChunks: {
+        // ignore non-index.min.js chunk
+        chunks: (chunk) => chunk.name === "index",
+        // Firefox addon store has a max file size of 4mb
+        maxSize: 4000000,
+      },
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        DEV_SERVER: false,
+        AMPLITUDE_KEY: JSON.stringify(env.AMPLITUDE_KEY),
+        SENTRY_KEY: JSON.stringify(env.SENTRY_KEY),
+      }),
+      ...(env.TRANSLATIONS
+        ? [
+            new I18nextWebpackPlugin({
+              async: true,
+              dest: "./",
+              extensions: [".ts", ".tsx"],
+              options: {
+                createOldCatalogs: false,
+                locales: LOCALES,
+                output: "src/popup/locales/$LOCALE/$NAMESPACE.json",
+                sort: true,
+                useKeysAsDefaultValue: true,
+              },
+            }),
+          ]
+        : []),
+    ],
+    // This if to fine tune logged output. Since this is an extension, not a
+    // webapp, we don't face the same bundle size constraints of the web.
+    performance: {
+      hints: false,
+    },
+  };
+};
 
 module.exports = (env) => merge(prodConfig(env), commonConfig(env));
