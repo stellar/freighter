1ba0450fb6a466570f2ab0318b838f1072f0ae47
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-08-18 08:44:51 -0600

    removes helper shim and fixes import pattern

--- @shared/helpers/soroban/server.ts
+++ @shared/helpers/soroban/server.ts
@@ -7,7 +7,6 @@ import {
   xdr,
   scValToNative,
 } from "soroban-client";
-import { captureException } from "@sentry/browser";
 
 export const simulateTx = async <ArgType>(
   tx: Transaction<Memo<MemoType>, Operation[]>,
@@ -19,14 +18,5 @@ export const simulateTx = async <ArgType>(
   }
   const result = results[0];
   const scVal = xdr.ScVal.fromXDR(result.xdr, "base64");
-  let convertedScVal: any;
-  try {
-    // handle a case where scValToNative doesn't properly handle scvString
-    convertedScVal = scVal.str().toString();
-    return convertedScVal;
-  } catch (e) {
-    console.error(e);
-    captureException(`Failed to convert SCVal to native val, ${e}`);
-  }
   return scValToNative(scVal);
 };
