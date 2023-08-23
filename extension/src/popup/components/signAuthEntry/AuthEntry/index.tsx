a06003e5462a7910ef34eefa6145cfc462760ad0
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-08-23 10:39:31 -0600

    adds buildInvocationTree to render formatted json for invocations and sub invocations

--- extension/src/popup/components/signAuthEntry/AuthEntry/index.tsx
+++ extension/src/popup/components/signAuthEntry/AuthEntry/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { xdr } from "soroban-client";
 
 import { SimpleBarWrapper } from "popup/basics/SimpleBarWrapper";
+import { buildInvocationTree } from "../invocation";
 import "./styles.scss";
 
 const authEntryParser = (entryXdr: string) =>
@@ -14,13 +15,17 @@ interface TransactionProps {
 
 export const AuthEntry = ({ authEntryXdr }: TransactionProps) => {
   const { t } = useTranslation();
+  const rootJson = buildInvocationTree(
+    authEntryParser(authEntryXdr).rootInvocation(),
+  );
+
   return (
     <div className="AuthEntry">
       <div className="AuthEntryHeader">{t("Authorization Entry")}</div>
       <div className="AuthEntryAttributes">
         <pre>
           <SimpleBarWrapper>
-            {JSON.stringify(authEntryParser(authEntryXdr), null, 2)}
+            {JSON.stringify(rootJson, null, 2)}
           </SimpleBarWrapper>
         </pre>
       </div>
