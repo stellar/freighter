971688218583d3eb91463deb226317209e616a02
Piyal Basu <pbasu235@gmail.com>
2024-02-16 18:08:08 -0500

    fix error in string translation (#1125)

--- extension/src/popup/components/sendPayment/SendConfirm/SubmitResult/index.tsx
+++ extension/src/popup/components/sendPayment/SendConfirm/SubmitResult/index.tsx
@@ -310,9 +310,8 @@ export const SubmitFail = () => {
             title={t("The destination account doesn’t exist")}
           >
             <div>
-              {t(
-                "The destination account doesn’t exist. Make sure it is a funded Stellar account and try again.",
-              )}
+              {t("The destination account doesn’t exist.")}{" "}
+              {t("Make sure it is a funded Stellar account and try again.")},
               <Link
                 isUnderline
                 variant="secondary"
