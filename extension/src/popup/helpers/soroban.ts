7d8ac654ff696687ec0b198d358e4b9ffab0fc65
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-02-20 09:49:15 -0700

    splits logic for auth entry invocations to account for contract creation

--- extension/src/popup/helpers/soroban.ts
+++ extension/src/popup/helpers/soroban.ts
@@ -327,18 +327,90 @@ export function pickTransfers(invocationTree: InvocationTree) {
 export function getInvocationDetails(
   invocation: xdr.SorobanAuthorizedInvocation,
 ) {
-  return [
+  const invocations = [
     getInvocationArgs(invocation),
     ...invocation.subInvocations().map(getInvocationArgs),
   ];
+  return invocations.filter(isInvocationArg);
 }
 
-function getInvocationArgs(invocation: xdr.SorobanAuthorizedInvocation) {
-  const _invocation = invocation.function().contractFn();
-  const contractId = StrKey.encodeContract(
-    _invocation.contractAddress().contractId(),
-  );
-  const fnName = _invocation.functionName().toString();
-  const args = _invocation.args();
-  return { fnName, contractId, args };
+export interface FnArgsInvoke {
+  type: "invoke";
+  fnName: string;
+  contractId: string;
+  args: xdr.ScVal[];
+}
+
+export interface FnArgsCreateWasm {
+  type: "wasm";
+  salt: string;
+  hash: string;
+  address: string;
+}
+
+export interface FnArgsCreateSac {
+  type: "sac";
+  asset: string;
+}
+
+type InvocationArgs = FnArgsInvoke | FnArgsCreateWasm | FnArgsCreateSac;
+
+const isInvocationArg = (
+  invocation: InvocationArgs | undefined,
+): invocation is InvocationArgs => !!invocation;
+
+function getInvocationArgs(
+  invocation: xdr.SorobanAuthorizedInvocation,
+): InvocationArgs | undefined {
+  const fn = invocation.function();
+
+  switch (fn.switch().value) {
+    // sorobanAuthorizedFunctionTypeContractFn
+    case 0: {
+      const _invocation = fn.contractFn();
+      const contractId = StrKey.encodeContract(
+        _invocation.contractAddress().contractId(),
+      );
+      const fnName = _invocation.functionName().toString();
+      const args = _invocation.args();
+      return { fnName, contractId, args, type: "invoke" };
+    }
+
+    // sorobanAuthorizedFunctionTypeCreateContractHostFn
+    case 1: {
+      const _invocation = fn.createContractHostFn();
+      const [exec, preimage] = [
+        _invocation.executable(),
+        _invocation.contractIdPreimage(),
+      ];
+
+      switch (exec.switch().value) {
+        // contractExecutableWasm
+        case 0: {
+          const details = preimage.fromAddress();
+
+          return {
+            type: "wasm",
+            salt: details.salt().toString("hex"),
+            hash: exec.wasmHash().toString("hex"),
+            address: Address.fromScAddress(details.address()).toString(),
+          };
+        }
+
+        // contractExecutableStellarAsset
+        case 1:
+          return {
+            type: "sac",
+            asset: Asset.fromOperation(preimage.fromAsset()).toString(),
+          };
+
+        default:
+          throw new Error(`unknown creation type: ${JSON.stringify(exec)}`);
+      }
+    }
+
+    default: {
+      return undefined;
+    }
+  }
 }
