4913c54bbe45fedf6d0cf82887abddd0576e92a9
Piyal Basu <pbasu235@gmail.com>
2024-02-21 17:13:00 -0500

    update account creation warning copy

--- extension/src/popup/components/sendPayment/SendTo/index.tsx
+++ extension/src/popup/components/sendPayment/SendTo/index.tsx
@@ -54,15 +54,15 @@ export const shouldAccountDoesntExistWarning = (
 
 export const AccountDoesntExistWarning = () => {
   const { t } = useTranslation();
-  const notificationTitle = t("The destination account doesn’t exist");
 
   return (
     <div className="SendTo__info-block">
-      <Notification variant="primary" title={notificationTitle}>
+      <Notification
+        variant="primary"
+        title={t("The destination account doesn’t exist")}
+      >
         <div>
-          {`${notificationTitle} ${t(
-            "Send at least 1 XLM to create account.",
-          )} `}
+          {t("Send at least 1 XLM to create account.")}
           <Link
             variant="secondary"
             href="https://developers.stellar.org/docs/tutorials/create-account/#create-account"
