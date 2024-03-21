aa6d3984c3d53b063051a6eac216c83a1b872146
Piyal Basu <pbasu235@gmail.com>
2024-03-21 17:06:02 -0400

    revert unnecessary changes

--- extension/playwright.config.ts
+++ extension/playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
