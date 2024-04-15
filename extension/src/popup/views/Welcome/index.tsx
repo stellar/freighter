0036a1d7c29cc1bfc1010aad63c11d1beb98cef1
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-04-15 14:43:43 -0600

    adds RN view for welcome page

--- extension/src/popup/views/Welcome/index.tsx
+++ extension/src/popup/views/Welcome/index.tsx
@@ -1,68 +1,99 @@
 import React from "react";
-import { Button, Card, Heading } from "@stellar/design-system";
-import { useTranslation } from "react-i18next";
-
-import { ROUTES } from "popup/constants/routes";
-import { navigateTo } from "popup/helpers/navigate";
-import { View } from "popup/basics/layout/View";
-
-import "./styles.scss";
+import {
+  Platform,
+  View as RNView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export const Welcome = () => {
-  const { t } = useTranslation();
+  const Component = Platform.select({
+    ios: () => {
+      const styles = StyleSheet.create({
+        button: {
+          marginTop: 24,
+          backgroundColor: "#7c66dc",
+          borderRadius: 4,
+          paddingVertical: 12,
+          paddingHorizontal: 20,
+        },
+        buttonText: {
+          color: "#fff",
+          fontSize: 16,
+          fontWeight: "bold",
+          textAlign: "center",
+        },
+        title: {
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 32,
+        },
+        importCard: {
+          backgroundColor: "black",
+          borderRadius: 4,
+          borderWidth: 1,
+          borderColor: "#2e2e2e",
+          paddingVertical: 24,
+          paddingHorizontal: 10,
+          marginTop: 24,
+          width: "95%",
+          height: 160,
+        },
+        createCard: {
+          backgroundColor: "#161618",
+          borderRadius: 4,
+          borderWidth: 1,
+          borderColor: "#2e2e2e",
+          paddingVertical: 24,
+          paddingHorizontal: 10,
+          marginTop: 64,
+          width: "95%",
+          height: 160,
+        },
+        cardTextTitle: {
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: "bold",
+        },
+        cardText: {
+          color: Colors.white,
+          fontSize: 14,
+          marginTop: 12,
+        },
+      });
+      return (
+        <React.Fragment>
+          <Text style={styles.title}>
+            Welcome! Is this your first time using Freighter?
+          </Text>
+          <RNView style={styles.createCard}>
+            <Text style={styles.cardTextTitle}>I'm new!</Text>
+            <Text style={styles.cardText}>I'm going to need a seed phrase</Text>
+            <TouchableOpacity
+              onPress={() => console.log("pressed create!")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Create Wallet</Text>
+            </TouchableOpacity>
+          </RNView>
+          <RNView style={styles.importCard}>
+            <Text style={styles.cardTextTitle}>I've done this before</Text>
+            <Text style={styles.cardText}>I have my 12 word seed phrase</Text>
+            <TouchableOpacity
+              onPress={() => console.log("pressed import!")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Import Wallet</Text>
+            </TouchableOpacity>
+          </RNView>
+        </React.Fragment>
+      );
+    },
+    web: () => import("./welcome-web") as any,
+  })!;
 
-  return (
-    <React.Fragment>
-      <View.Header />
-      <View.Content>
-        <div className="Welcome__column">
-          <div className="Welcome__centered-screen">
-            <Heading as="h1" size="lg">
-              {t("Welcome! Is this your first time using Freighter?")}
-            </Heading>
-          </div>
-          <div className="Welcome__row-screen">
-            <div className="Welcome__half-screen">
-              <Card variant="secondary">
-                <div className="Welcome__heading--small">{t("I’m new!")}</div>
-                <div className="Welcome__text">
-                  {t("I’m going to need a seed phrase")}
-                </div>
-                <div className="Welcome__button-container">
-                  <Button
-                    size="md"
-                    isFullWidth
-                    variant="primary"
-                    onClick={() => navigateTo(ROUTES.accountCreator)}
-                  >
-                    {t("Create Wallet")}
-                  </Button>
-                </div>
-              </Card>
-            </div>
-            <div className="Welcome__half-screen Welcome__import">
-              <Card>
-                <div className="Welcome__heading--small">
-                  {t("I’ve done this before")}
-                </div>
-                <div className="Welcome__text">
-                  {t("I have my 12 word seed phrase")}
-                </div>
-                <div className="Welcome__button-container">
-                  <Button
-                    size="md"
-                    isFullWidth
-                    variant="secondary"
-                    onClick={() => navigateTo(ROUTES.recoverAccount)}
-                  >
-                    {t("Import Wallet")}
-                  </Button>
-                </div>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </View.Content>
-    </React.Fragment>
-  );
+  return <Component />;
 };
---
+++ extension/src/popup/views/Welcome/welcome-web.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Platform } from "react-native";
+
+if (Platform.OS === "web") {
+}
+
+import { Button, Card, Heading } from "@stellar/design-system";
+import { useTranslation } from "react-i18next";
+
+import { ROUTES } from "popup/constants/routes";
+import { navigateTo } from "popup/helpers/navigate";
+import { View } from "popup/basics/layout/View";
+
+import "../styles.scss";
+
+const Welcome = () => {
+  const { t } = useTranslation();
+  return (
+    <React.Fragment>
+      <View.Header />
+      <View.Content>
+        <div className="Welcome__column">
+          <div className="Welcome__centered-screen">
+            <Heading as="h1" size="lg">
+              {t("Welcome! Is this your first time using Freighter?")}
+            </Heading>
+          </div>
+          <div className="Welcome__row-screen">
+            <div className="Welcome__half-screen">
+              <Card variant="secondary">
+                <div className="Welcome__heading--small">{t("I’m new!")}</div>
+                <div className="Welcome__text">
+                  {t("I’m going to need a seed phrase")}
+                </div>
+                <div className="Welcome__button-container">
+                  <Button
+                    size="md"
+                    isFullWidth
+                    variant="primary"
+                    onClick={() => navigateTo(ROUTES.accountCreator)}
+                  >
+                    {t("Create Wallet")}
+                  </Button>
+                </div>
+              </Card>
+            </div>
+            <div className="Welcome__half-screen Welcome__import">
+              <Card>
+                <div className="Welcome__heading--small">
+                  {t("I’ve done this before")}
+                </div>
+                <div className="Welcome__text">
+                  {t("I have my 12 word seed phrase")}
+                </div>
+                <div className="Welcome__button-container">
+                  <Button
+                    size="md"
+                    isFullWidth
+                    variant="secondary"
+                    onClick={() => navigateTo(ROUTES.recoverAccount)}
+                  >
+                    {t("Import Wallet")}
+                  </Button>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </View.Content>
+    </React.Fragment>
+  );
+};
+
+export default Welcome;
