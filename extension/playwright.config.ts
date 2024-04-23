bc07fa298949b6654ec277ed8225070e41e8abfe
Piyal Basu <pbasu235@gmail.com>
2024-04-22 23:43:55 -0400

    adding snapshots and husky pre-push

--- extension/playwright.config.ts
+++ extension/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: false, // !!process.env.CI,
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
