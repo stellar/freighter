33004a05d33a641600c67f5cc2565c5b50121659
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-10-10 10:19:33 -0600

    accounts for a source account in op having no balance at tx sign

--- extension/src/popup/views/SignTransaction/index.tsx
+++ extension/src/popup/views/SignTransaction/index.tsx
@@ -237,9 +237,9 @@ export const SignTransaction = () => {
 
   const hasBalance =
     hasLoadedBalances && accountBalanceStatus !== ActionStatus.ERROR;
-  const hasEnoughXlm = accountBalances.balances?.native.available.gt(
-    stroopToXlm(_fee as string),
-  );
+  const hasEnoughXlm =
+    accountBalances.balances?.native &&
+    accountBalances.balances?.native.available.gt(stroopToXlm(_fee as string));
   if (
     hasBalance &&
     currentAccount.publicKey &&
