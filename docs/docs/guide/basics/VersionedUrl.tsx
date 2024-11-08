181c8a33d7209664a6ccdb6d5a6a6dd76ca89a54
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2024-11-08 12:53:04 -0300

    The text is referring a code with `{version}` tag

--- docs/docs/guide/basics/VersionedUrl.tsx
+++ docs/docs/guide/basics/VersionedUrl.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import packageJson from "../../../../@stellar/freighter-api/package.json";
 
 export const VersionedUrl = () => (
   <p>
-    <code>{`<head><script src='https://cdnjs.cloudflare.com/ajax/libs/stellar-freighter-api/${packageJson.version}/index.min.js' /></head>`}</code>
+    <code>{`<head><script src='https://cdnjs.cloudflare.com/ajax/libs/stellar-freighter-api/{version}/index.min.js' /></head>`}</code>
   </p>
 );
