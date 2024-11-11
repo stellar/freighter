217eda9119a391419cc064295b68d60aa0747cfb
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2024-11-11 11:06:25 -0300

    Revert VersionedUrl change

--- docs/docs/guide/basics/VersionedUrl.tsx
+++ docs/docs/guide/basics/VersionedUrl.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import packageJson from "../../../../@stellar/freighter-api/package.json";
 
 export const VersionedUrl = () => (
   <p>
-    <code>{`<head><script src='https://cdnjs.cloudflare.com/ajax/libs/stellar-freighter-api/{version}/index.min.js' /></head>`}</code>
+    <code>{`<head><script src='https://cdnjs.cloudflare.com/ajax/libs/stellar-freighter-api/${packageJson.version}/index.min.js' /></head>`}</code>
   </p>
 );
