dcf8ca5300945b74116b6ccccf6c80989e82348c
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-11-13 12:18:27 -0700

    adds test for create contract v1 in sub invoke

--- extension/src/popup/helpers/__tests__/soroban.test.js
+++ extension/src/popup/helpers/__tests__/soroban.test.js
@@ -4,7 +4,33 @@ import { getInvocationArgs } from "../soroban";
 import { TEST_PUBLIC_KEY } from "popup/__testHelpers__";
 
 describe("getInvocationArgs", () => {
-  it("can render a create contract v2", async () => {
+  it("can render a create contract v1", () => {
+    const assetCode = "KHL";
+    const assetType = new xdr.AlphaNum4({
+      assetCode: Buffer.from(assetCode),
+      issuer: Keypair.fromPublicKey(TEST_PUBLIC_KEY).xdrAccountId(),
+    });
+    const args = new xdr.CreateContractArgs({
+      contractIdPreimage: xdr.ContractIdPreimage.contractIdPreimageFromAsset(
+        xdr.Asset.assetTypeCreditAlphanum4(assetType),
+      ),
+      executable: xdr.ContractExecutable.contractExecutableStellarAsset(),
+    });
+    const authorizedFn =
+      xdr.SorobanAuthorizedFunction.sorobanAuthorizedFunctionTypeCreateContractHostFn(
+        args,
+      );
+    const authorizedInvocation = new xdr.SorobanAuthorizedInvocation({
+      function: authorizedFn,
+      subInvocations: [],
+    });
+    const invocationArgs = getInvocationArgs(authorizedInvocation);
+    expect(invocationArgs).toEqual({
+      type: "sac",
+      asset: `${assetCode}:${TEST_PUBLIC_KEY}`,
+    });
+  });
+  it("can render a create contract v2", () => {
     const assetCode = "KHL";
     const assetType = new xdr.AlphaNum4({
       assetCode: Buffer.from(assetCode),
