fc72dfa0ab530317431934afafba264ee294afeb
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2024-12-02 17:01:34 -0300

    Redesign text on UnlockAccount

--- extension/src/popup/views/UnlockAccount/index.tsx
+++ extension/src/popup/views/UnlockAccount/index.tsx
@@ -3,7 +3,7 @@ import get from "lodash/get";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useHistory } from "react-router-dom";
 import { Field, Form, Formik, FieldProps } from "formik";
-import { Button, Heading, Input } from "@stellar/design-system";
+import { Button, Text, Input } from "@stellar/design-system";
 import { useTranslation } from "react-i18next";
 
 import { ROUTES } from "popup/constants/routes";
@@ -48,49 +48,57 @@ export const UnlockAccount = () => {
       <View.Header />
       <View.Content alignment="center">
         <div className="UnlockAccount">
-          <Heading as="h1" size="lg">
-            {t("A Stellar wallet for every website")}
-          </Heading>
-          <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-            {({ dirty, isSubmitting, isValid, errors, touched }) => (
-              <Form>
-                <Field name="password">
-                  {({ field }: FieldProps) => (
-                    <Input
-                      id="password-input"
-                      isPassword
-                      fieldSize="md"
-                      autoComplete="off"
-                      placeholder={t("Enter Password")}
-                      error={
-                        authError ||
-                        (errors.password && touched.password
-                          ? errors.password
-                          : "")
-                      }
-                      {...field}
-                    />
-                  )}
-                </Field>
+          <div className="UnlockAccount__wrapper">
+            <Text as="div" size="sm" addlClassName="UnlockAccount__title">
+              {t("Welcome back!")}
+            </Text>
+            <Text as="div" size="sm" addlClassName="UnlockAccount__subtitle">
+              {t("Enter password to unlock Freighter")}
+            </Text>
 
-                <div className="UnlockAccount__spacer-small" />
+            <div className="UnlockAccount__spacer-big" />
 
-                <Button
-                  size="md"
-                  isFullWidth
-                  variant="secondary"
-                  isLoading={isSubmitting}
-                  disabled={!(dirty && isValid)}
-                >
-                  {t("Login")}
-                </Button>
-              </Form>
-            )}
-          </Formik>
+            <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+              {({ dirty, isSubmitting, isValid, errors, touched }) => (
+                <Form>
+                  <Field name="password">
+                    {({ field }: FieldProps) => (
+                      <Input
+                        id="password-input"
+                        isPassword
+                        fieldSize="md"
+                        autoComplete="off"
+                        placeholder={t("Enter Password")}
+                        error={
+                          authError ||
+                          (errors.password && touched.password
+                            ? errors.password
+                            : "")
+                        }
+                        {...field}
+                      />
+                    )}
+                  </Field>
+
+                  <div className="UnlockAccount__spacer-small" />
+
+                  <Button
+                    size="md"
+                    isFullWidth
+                    variant="secondary"
+                    isLoading={isSubmitting}
+                    disabled={!(dirty && isValid)}
+                  >
+                    {t("Login")}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </div>
         </div>
       </View.Content>
 
-      <View.Footer hasExtraPaddingBottom customGap="0.25rem">
+      <View.Footer customGap="0.5rem">
         <div className="UnlockAccount__footer-label">
           {t("Want to add another account?")}
         </div>
@@ -105,8 +113,6 @@ export const UnlockAccount = () => {
         >
           {t("Import using account seed phrase")}
         </Button>
-
-        <div className="UnlockAccount__spacer" />
 
         <Button
           size="md"
