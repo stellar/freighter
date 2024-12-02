baaccbdb1e81b31877881f076e2d477b587c1555
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2024-12-02 17:36:43 -0300

    Add identicon UI on UnlockAccount

--- extension/src/popup/views/UnlockAccount/index.tsx
+++ extension/src/popup/views/UnlockAccount/index.tsx
@@ -6,9 +6,12 @@ import { Field, Form, Formik, FieldProps } from "formik";
 import { Button, Text, Input } from "@stellar/design-system";
 import { useTranslation } from "react-i18next";
 
+import { newTabHref } from "helpers/urls";
+import { truncatedPublicKey } from "helpers/stellar";
+
+import { IdenticonImg } from "popup/components/identicons/IdenticonImg";
 import { ROUTES } from "popup/constants/routes";
 import { openTab } from "popup/helpers/navigate";
-import { newTabHref } from "helpers/urls";
 import { View } from "popup/basics/layout/View";
 import {
   confirmPassword,
@@ -24,6 +27,8 @@ export const UnlockAccount = () => {
   const from = get(location, "state.from.pathname", "") as ROUTES;
   const queryParams = get(location, "search", "");
   const destination = from || ROUTES.account;
+
+  const gAddress = "GBJQABA6Q5GWHVARXAYZALLPABXJITRTO6ZY4L43TY27X6WYB6VLMTXC";
 
   const dispatch = useDispatch();
   const authError = useSelector(authErrorSelector);
@@ -49,57 +54,68 @@ export const UnlockAccount = () => {
       <View.Content alignment="center">
         <div className="UnlockAccount">
           <div className="UnlockAccount__wrapper">
-            <Text as="div" size="sm" addlClassName="UnlockAccount__title">
-              {t("Welcome back!")}
-            </Text>
-            <Text as="div" size="sm" addlClassName="UnlockAccount__subtitle">
-              {t("Enter password to unlock Freighter")}
+            <div className="UnlockAccount__identicon">
+              <IdenticonImg publicKey={gAddress} />
+            </div>
+
+            <Text as="div" size="xs" addlClassName="UnlockAccount__gray11">
+              {truncatedPublicKey(gAddress)}
             </Text>
 
             <div className="UnlockAccount__spacer-big" />
 
-            <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-              {({ dirty, isSubmitting, isValid, errors, touched }) => (
-                <Form>
-                  <Field name="password">
-                    {({ field }: FieldProps) => (
-                      <Input
-                        id="password-input"
-                        isPassword
-                        fieldSize="md"
-                        autoComplete="off"
-                        placeholder={t("Enter Password")}
-                        error={
-                          authError ||
-                          (errors.password && touched.password
-                            ? errors.password
-                            : "")
-                        }
-                        {...field}
-                      />
-                    )}
-                  </Field>
+            <Text as="div" size="sm">
+              {t("Welcome back!")}
+            </Text>
 
-                  <div className="UnlockAccount__spacer-small" />
+            <Text as="div" size="sm" addlClassName="UnlockAccount__gray11">
+              {t("Enter password to unlock Freighter")}
+            </Text>
 
-                  <Button
-                    size="md"
-                    isFullWidth
-                    variant="secondary"
-                    isLoading={isSubmitting}
-                    disabled={!(dirty && isValid)}
-                  >
-                    {t("Login")}
-                  </Button>
-                </Form>
-              )}
-            </Formik>
+            <div className="UnlockAccount__formik">
+              <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+                {({ dirty, isSubmitting, isValid, errors, touched }) => (
+                  <Form>
+                    <Field name="password">
+                      {({ field }: FieldProps) => (
+                        <Input
+                          id="password-input"
+                          isPassword
+                          fieldSize="md"
+                          autoComplete="off"
+                          placeholder={t("Enter Password")}
+                          error={
+                            authError ||
+                            (errors.password && touched.password
+                              ? errors.password
+                              : "")
+                          }
+                          {...field}
+                        />
+                      )}
+                    </Field>
+
+                    <div className="UnlockAccount__spacer-small" />
+
+                    <Button
+                      size="md"
+                      isFullWidth
+                      variant="secondary"
+                      isLoading={isSubmitting}
+                      disabled={!(dirty && isValid)}
+                    >
+                      {t("Login")}
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+            </div>
           </div>
         </div>
       </View.Content>
 
       <View.Footer customGap="0.5rem">
-        <div className="UnlockAccount__footer-label">
+        <div className="UnlockAccount__footer-label UnlockAccount__gray11">
           {t("Want to add another account?")}
         </div>
 
