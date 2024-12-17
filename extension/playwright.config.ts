18b34d129ff9ec03d6f03c8f2d6b31b9f3458b62
Piyal Basu <pbasu235@gmail.com>
2024-12-17 12:27:34 -0500

    all multiple workers when not in INTEGRATION MODE

--- extension/playwright.config.ts
+++ extension/playwright.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Opt out of parallel tests on CI if we're in INTEGRATION MODE */
+  workers: process.env.IS_INTEGRATION_MODE ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
