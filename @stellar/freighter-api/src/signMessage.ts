d5649289b64d78ad245ddf06b56d5666e1b5666f
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-12-19 09:31:34 -0700

    add v3 and v4 response types for signMessage, return v4 response according to package version

--- @stellar/freighter-api/src/signMessage.ts
+++ @stellar/freighter-api/src/signMessage.ts
@@ -1,7 +1,25 @@
+import packageJson from "../package.json";
+import semver from "semver";
+import { Buffer } from "buffer";
+
 import { submitMessage } from "@shared/api/external";
 import { FreighterApiError } from "@shared/api/types";
 import { FreighterApiNodeError } from "@shared/api/helpers/extensionMessaging";
 import { isBrowser } from ".";
+
+type SignMessageV3Response = {
+  signedMessage: Buffer | null;
+  signerAddress: string;
+} & {
+  error?: FreighterApiError;
+};
+
+type SignMessageV4Response = {
+  signedMessage: string;
+  signerAddress: string;
+} & {
+  error?: FreighterApiError;
+};
 
 export const signMessage = async (
   message: string,
@@ -9,16 +27,20 @@ export const signMessage = async (
     networkPassphrase?: string;
     address?: string;
   }
-): Promise<
-  { signedMessage: Buffer | null; signerAddress: string } & {
-    error?: FreighterApiError;
-  }
-> => {
+): Promise<SignMessageV3Response | SignMessageV4Response> => {
   if (isBrowser) {
     const req = await submitMessage(message, opts);
 
     if (req.error) {
       return { signedMessage: null, signerAddress: "", error: req.error };
+    }
+
+    if (semver.gte(packageJson.version, "4.0.0") && req.signedMessage) {
+      const signedMessage = Buffer.from(req.signedMessage).toString("base64");
+      return {
+        signedMessage,
+        signerAddress: req.signerAddress,
+      };
     }
 
     return {
