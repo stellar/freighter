e9ebd42d08eab9bd7f7dad35c8dc51c86af060cf
Aristides Staffieri <aristides.staffieri@stellar.org>
2025-01-08 10:23:40 -0700

    test the cross platform compilation

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,5 @@
+import Button from "@/components/ui/Button";
+
+export default function RootLayout() {
+  return <Button />;
+}
---
+++ mobile/components/ui/Button/web.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function WebButton() {
+  return (
+    <button onClick={() => alert("Web Button Clicked")}>Click Me (Web)</button>
+  );
+}
