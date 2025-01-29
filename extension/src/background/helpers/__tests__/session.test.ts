90e165e07515f1d2696e5cf1d3b7c87fe01d6d7c
Piyal Basu <pbasu235@gmail.com>
2025-01-29 14:47:37 -0500

    remove npm package and add unit tests

---
+++ extension/src/background/helpers/__tests__/session.test.ts
@@ -0,0 +1,66 @@
+import {
+  deriveKeyFromString,
+  encryptHashString,
+  decryptHashString,
+} from "../session";
+
+describe("session", () => {
+  it("should be able to encrypt and decrypt a string", async () => {
+    const password = "password";
+    const privateKey = "privateKey";
+
+    const { key, iv } = await deriveKeyFromString(password);
+
+    const encryptedPrivateKey = await encryptHashString({
+      str: privateKey,
+      keyObject: { key, iv },
+    });
+
+    console.log(encryptedPrivateKey);
+
+    const decryptedPrivateKey = await decryptHashString({
+      hash: encryptedPrivateKey,
+      keyObject: { key, iv },
+    });
+
+    expect(decryptedPrivateKey).toEqual(privateKey);
+  });
+  it("should be able to encrypt and decrypt a very long string with different characters", async () => {
+    const password =
+      "passwordpasswordpasswordpasswordpasswordpassworw21w1w1@@@@dpasswordpasswordpasswordpcxassad@@@@asswordpasswordpasswordpasswordpassword";
+    const privateKey = "privateKeyprivateKeyprivateKeyprivateKey";
+
+    const { key, iv } = await deriveKeyFromString(password);
+
+    const encryptedPrivateKey = await encryptHashString({
+      str: privateKey,
+      keyObject: { key, iv },
+    });
+
+    const decryptedPrivateKey = await decryptHashString({
+      hash: encryptedPrivateKey,
+      keyObject: { key, iv },
+    });
+
+    expect(decryptedPrivateKey).toEqual(privateKey);
+  });
+  it("should be able to encrypt and decrypt an empty string", async () => {
+    // this is an edge case and should never happen, but want to make sure this does not throw an error
+    const password = "";
+    const privateKey = "";
+
+    const { key, iv } = await deriveKeyFromString(password);
+
+    const encryptedPrivateKey = await encryptHashString({
+      str: privateKey,
+      keyObject: { key, iv },
+    });
+
+    const decryptedPrivateKey = await decryptHashString({
+      hash: encryptedPrivateKey,
+      keyObject: { key, iv },
+    });
+
+    expect(decryptedPrivateKey).toEqual(privateKey);
+  });
+});
