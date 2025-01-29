0e4a24bbaba1eaee160f5e2562d0c7687bb86977
Aristides Staffieri <aristides.staffieri@stellar.org>
2025-01-29 12:03:05 -0700

    restores label in account assets

--- extension/src/popup/components/account/AccountAssets/index.tsx
+++ extension/src/popup/components/account/AccountAssets/index.tsx
@@ -233,19 +233,23 @@ export const AccountAssets = ({
           key: "",
         };
         let code = "";
+        let amountUnit;
         if (rb.liquidityPoolId) {
           isLP = true;
           code = getLPShareCode(rb.reserves as Horizon.HorizonApi.Reserve[]);
+          amountUnit = "shares";
         } else if (rb.contractId && "symbol" in rb) {
           issuer = {
             key: rb.contractId,
           };
           code = rb.symbol;
+          amountUnit = rb.symbol;
         } else {
           if ("issuer" in rb.token && rb.token) {
             issuer = rb.token.issuer;
           }
           code = rb.token.code;
+          amountUnit = rb.token.code;
         }
 
         const canonicalAsset = getCanonicalFromAsset(code, issuer?.key);
@@ -281,7 +285,7 @@ export const AccountAssets = ({
             </div>
             <div className="AccountAssets__copy-right">
               <div className="asset-amount" data-testid="asset-amount">
-                {formatAmount(amountVal)}
+                {formatAmount(amountVal)} <span>{amountUnit}</span>
               </div>
             </div>
           </div>
