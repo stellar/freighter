3782f21f197fd95e97b9c12f274de93fb957dfdd
Aristides Staffieri <aristides.staffieri@stellar.org>
2025-03-06 12:54:09 -0500

    updates notificaton modal to match latest mocks

---
+++ extension/src/popup/components/Notification/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Icon } from "@stellar/design-system";
+import classNames from "classnames";
+
+// import IconWarning from "../../assets/icon-warning.svg";
+// import IconShieldPlus from "../../assets/icon-shield-plus.svg";
+
+import "./styles.scss";
+
+type NotificationType = "warning" | "info";
+
+export const Notification = (props: {
+  description: string;
+  type: NotificationType;
+}) => {
+  const { t } = useTranslation();
+
+  const getIcon = (type: NotificationType) => {
+    switch (type) {
+      case "warning":
+        return <Icon.AlertTriangle className="warning" />;
+      case "info":
+      default:
+        return <Icon.AlertOctagon className="info" />;
+    }
+  };
+
+  const classes = classNames("NotificationModal__box", {
+    "NotificationModal__box--isWarning": props.type === "warning",
+    "NotificationModal__box--isInfo": props.type === "info",
+  });
+
+  return (
+    <div className={classes} data-testid="NotificationModal">
+      <div className="NotificationModal__box__content">
+        <div className="NotificationModal__Icon">{getIcon(props.type)}</div>
+        <div className="NotificationModal__alert">
+          <div className="NotificationModal__description">
+            {t(props.description)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
