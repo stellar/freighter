452c732a8d189395680c3dab81385dfdfb0747c9
Piyal Basu <pbasu235@gmail.com>
2025-04-28 15:08:29 -0400

    add e2e test for account-history

--- extension/e2e-tests/accountHistory.test.ts
+++ extension/e2e-tests/accountHistory.test.ts
@@ -61,3 +61,94 @@ test("View failed transaction", async ({ page, extensionId }) => {
     screenshot: "failed-transaction.png",
   });
 });
+test("Hide create claimable balance spam", async ({ page, extensionId }) => {
+  await page.route("*/**/account-history/*", async (route) => {
+    const json = [
+      {
+        amount: "0.0010000",
+        asset_code: "USDC",
+        asset_issuer:
+          "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5",
+        asset_type: "credit_alphanum4",
+        created_at: "2025-03-21T22:28:46Z",
+        from: "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        id: "164007621169153",
+        paging_token: "164007621169153",
+        source_account:
+          "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        to: "GCKUVXILBNYS4FDNWCGCYSJBY2PBQ4KAW2M5CODRVJPUFM62IJFH67J2",
+        transaction_attr: {},
+        transaction_hash:
+          "686601028de9ddf40a1c24461a6a9c0415d60a39255c35eccad0b52ac1e700a5",
+        transaction_successful: true,
+        type: "payment",
+        type_i: 1,
+      },
+      {
+        amount: "0.0010000",
+        asset_code: "USDC",
+        asset_issuer:
+          "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5",
+        asset_type: "credit_alphanum4",
+        created_at: "2025-03-21T22:28:46Z",
+        from: "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        id: "164007621169153",
+        paging_token: "164007621169153",
+        source_account:
+          "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        to: "GCKUVXILBNYS4FDNWCGCYSJBY2PBQ4KAW2M5CODRVJPUFM62IJFH67J2",
+        transaction_attr: {},
+        transaction_hash:
+          "686601028de9ddf40a1c24461a6a9c0415d60a39255c35eccad0b52ac1e700a5",
+        transaction_successful: true,
+        type: "payment",
+        type_i: 1,
+      },
+      {
+        amount: "0.0010000",
+        asset: "USDC",
+        created_at: "2025-03-19T22:28:46Z",
+        id: "164007621169153",
+        paging_token: "164007621169153",
+        source_account:
+          "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        transaction_attr: {
+          operation_count: 100,
+          successful: true,
+        },
+        transaction_hash:
+          "686601028de9ddf40a1c24461a6a9c0415d60a39255c35eccad0b52ac1e700a5",
+        transaction_successful: true,
+        type: "create_claimable_balance",
+        type_i: 14,
+      },
+      {
+        amount: "0.0010000",
+        asset: "USDC",
+        created_at: "2025-03-18T22:28:46Z",
+        id: "164007621169153",
+        paging_token: "164007621169153",
+        source_account:
+          "GDF32CQINROD3E2LMCGZUDVMWTXCJFR5SBYVRJ7WAAIAS3P7DCVWZEFY",
+        transaction_attr: {
+          operation_count: 100,
+          successful: true,
+        },
+        transaction_hash:
+          "686601028de9ddf40a1c24461a6a9c0415d60a39255c35eccad0b52ac1e700a5",
+        transaction_successful: true,
+        type: "create_claimable_balance",
+        type_i: 14,
+      },
+    ];
+    await route.fulfill({ json });
+  });
+
+  test.slow();
+  await loginAndFund({ page, extensionId });
+  await page.getByTestId("BottomNav-link-account-history").click();
+  await expect(page.getByTestId("AppHeaderPageTitle")).toHaveText("History");
+  const historyItems = page.getByTestId("history-item");
+  console.log(historyItems);
+  expect(historyItems).toHaveCount(2);
+});
