eeee481620a4c236a4c707624a072726cb27c51d
Aristides Staffieri <aristides.staffieri@stellar.org>
2025-07-16 15:06:46 -0600

    shows truncated wasm hash for contract upload details in sign tx

--- extension/src/popup/components/signTransaction/Operations/KeyVal/index.tsx
+++ extension/src/popup/components/signTransaction/Operations/KeyVal/index.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import {
   Asset,
   Claimant,
+  hash,
   LiquidityPoolAsset,
   nativeToScVal,
   Operation,
@@ -668,14 +669,17 @@ export const KeyValueInvokeHostFn = ({
       }
 
       case xdr.HostFunctionType.hostFunctionTypeUploadContractWasm(): {
-        const wasm = hostfn.wasm().toString();
+        const wasmHash = hash(hostfn.value() as Buffer);
         return (
           <>
             <KeyValueList
               operationKey={t("Invocation Type")}
               operationValue="Upload Contract Wasm"
             />
-            <KeyValueList operationKey={t("wasm")} operationValue={wasm} />
+            <KeyValueList
+              operationKey={t("Wasm Hash")}
+              operationValue={truncateString(wasmHash.toString("hex"), 8)}
+            />
           </>
         );
       }
