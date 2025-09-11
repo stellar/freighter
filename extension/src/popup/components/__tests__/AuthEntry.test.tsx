e3c414a24a2c9fdd89f33bad3881d631e9d8b626
Piyal Basu <pbasu235@gmail.com>
2025-09-11 15:25:07 -0400

    add tests for create contract v1 and invoke contract

--- extension/src/popup/components/__tests__/AuthEntry.test.tsx
+++ extension/src/popup/components/__tests__/AuthEntry.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, waitFor, screen, fireEvent } from "@testing-library/react";
-import { Address, Keypair, xdr } from "stellar-sdk";
+import { Address, Keypair, ScInt, StrKey, xdr } from "stellar-sdk";
 
 import { mockAccounts, TEST_PUBLIC_KEY, Wrapper } from "popup/__testHelpers__";
 import { AuthEntries } from "../AuthEntry";
@@ -30,6 +30,62 @@ describe("AuthEntry", () => {
         },
       });
     });
+
+  it("renders auth entries for create contract v1", async () => {
+    const assetCode = "KHL1";
+    const assetType = new xdr.AlphaNum4({
+      assetCode: Buffer.from(assetCode),
+      issuer: Keypair.fromPublicKey(TEST_PUBLIC_KEY).xdrAccountId(),
+    });
+
+    const args = new xdr.CreateContractArgs({
+      contractIdPreimage: xdr.ContractIdPreimage.contractIdPreimageFromAsset(
+        xdr.Asset.assetTypeCreditAlphanum4(assetType),
+      ),
+      executable: xdr.ContractExecutable.contractExecutableStellarAsset(),
+    });
+
+    const authorizedFn =
+      xdr.SorobanAuthorizedFunction.sorobanAuthorizedFunctionTypeCreateContractHostFn(
+        args,
+      );
+    const authorizedInvocation = new xdr.SorobanAuthorizedInvocation({
+      function: authorizedFn,
+      subInvocations: [],
+    });
+
+    render(
+      <Wrapper
+        routes={[ROUTES.reviewAuthorization]}
+        state={{
+          auth: {
+            error: null,
+            applicationState: APPLICATION_STATE.PASSWORD_CREATED,
+            TEST_PUBLIC_KEY,
+            allAccounts: mockAccounts,
+            hasPrivateKey: true,
+          },
+          settings: {
+            networkDetails: TESTNET_NETWORK_DETAILS,
+            networksList: DEFAULT_NETWORKS,
+            isSorobanPublicEnabled: true,
+            isRpcHealthy: true,
+          },
+        }}
+      >
+        <AuthEntries invocations={[authorizedInvocation]} />
+      </Wrapper>,
+    );
+    await waitFor(() => screen.getAllByTestId("AuthEntryContainer"));
+    await fireEvent.click(screen.getByTestId("AuthEntryBtn"));
+    await waitFor(() => screen.getAllByTestId("AuthEntryContent"));
+
+    expect(screen.getByTestId("AuthEntryBtn__Title")).toHaveTextContent(
+      "Contract creation",
+    );
+
+    expect(getContractSpecSpy).not.toHaveBeenCalled();
+  });
 
   it("renders auth entries for create contract v2", async () => {
     const assetCode = "KHL1";
@@ -94,6 +150,73 @@ describe("AuthEntry", () => {
     expect(parameterKeys[0]).toHaveTextContent("");
 
     expect(parameterValues).toHaveLength(1);
+    expect(parameterValues[0]).toHaveTextContent(TEST_PUBLIC_KEY);
+  });
+
+  it("renders auth entries for invoke contract args", async () => {
+    const CONTRACT = "CA3D5KRYM6CB7OWQ6TWYRR3Z4T7GNZLKERYNZGGA5SOAOPIFY6YQGAXE";
+    const args = new xdr.InvokeContractArgs({
+      functionName: Buffer.from("transfer"),
+      args: [
+        new Address(TEST_PUBLIC_KEY).toScVal(),
+        new Address(TEST_PUBLIC_KEY).toScVal(),
+        new ScInt(100).toI128(),
+      ],
+      contractAddress: xdr.ScAddress.scAddressTypeContract(
+        StrKey.decodeContract(CONTRACT) as any,
+      ),
+    });
+
+    const authorizedFn =
+      xdr.SorobanAuthorizedFunction.sorobanAuthorizedFunctionTypeContractFn(
+        args,
+      );
+    const authorizedInvocation = new xdr.SorobanAuthorizedInvocation({
+      function: authorizedFn,
+      subInvocations: [],
+    });
+
+    render(
+      <Wrapper
+        routes={[ROUTES.reviewAuthorization]}
+        state={{
+          auth: {
+            error: null,
+            applicationState: APPLICATION_STATE.PASSWORD_CREATED,
+            TEST_PUBLIC_KEY,
+            allAccounts: mockAccounts,
+            hasPrivateKey: true,
+          },
+          settings: {
+            networkDetails: TESTNET_NETWORK_DETAILS,
+            networksList: DEFAULT_NETWORKS,
+            isSorobanPublicEnabled: true,
+            isRpcHealthy: true,
+          },
+        }}
+      >
+        <AuthEntries invocations={[authorizedInvocation]} />
+      </Wrapper>,
+    );
+    await waitFor(() => screen.getAllByTestId("AuthEntryContainer"));
+    await fireEvent.click(screen.getByTestId("AuthEntryBtn"));
+    await waitFor(() => screen.getAllByTestId("AuthEntryContent"));
+
+    expect(screen.getByTestId("AuthEntryBtn__Title")).toHaveTextContent(
+      "transfer",
+    );
+
+    expect(getContractSpecSpy).not.toHaveBeenCalled();
+
+    const parameterKeys = screen.getAllByTestId("ParameterKey");
+    const parameterValues = screen.getAllByTestId("ParameterValue");
+
+    expect(parameterKeys).toHaveLength(3);
+    expect(parameterKeys[0]).toHaveTextContent("");
+    expect(parameterKeys[1]).toHaveTextContent("");
+    expect(parameterKeys[2]).toHaveTextContent("");
+
+    expect(parameterValues).toHaveLength(3);
     expect(parameterValues[0]).toHaveTextContent(TEST_PUBLIC_KEY);
   });
 });
