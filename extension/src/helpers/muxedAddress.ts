9697b7dd90531da87817c3a604a7be7866ceeab8
leofelix077 <leonardoaalf077@hotmail.com>
2026-01-06 16:54:23 -0300

    fix tests for loading accounts

--- extension/src/helpers/muxedAddress.ts
+++ extension/src/helpers/muxedAddress.ts
@@ -100,15 +100,16 @@ export function getMemoDisabledState(
 ): MemoDisabledState {
   const { targetAddress, contractId, contractSupportsMuxed, t } = params;
 
-  if (!contractId) {
-    return { isMemoDisabled: false, memoDisabledMessage: undefined };
-  }
-
+  // Disable memo for all M addresses (memo is encoded in the address)
   if (isMuxedAccount(targetAddress)) {
     return {
       isMemoDisabled: true,
       memoDisabledMessage: t("Memo is disabled for this transaction"),
     };
+  }
+
+  if (!contractId) {
+    return { isMemoDisabled: false, memoDisabledMessage: undefined };
   }
 
   // For tokens without Soroban mux support, disable memo
