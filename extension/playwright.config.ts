0176d414b685870a4f51ff678401c8c2caadc84c
Piyal Basu <pbasu235@gmail.com>
2026-01-28 22:17:57 -0500

    restore missing tests

--- extension/playwright.config.ts
+++ extension/playwright.config.ts
@@ -19,7 +19,8 @@ export default defineConfig({
   retries: 5,
   /* Fail the build if any test fails after retries */
   maxFailures: 1,
-
+  /* In integration mode, run tests sequentially to avoid conflicts */
+  workers: process.env.IS_INTEGRATION_MODE ? 1 : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
