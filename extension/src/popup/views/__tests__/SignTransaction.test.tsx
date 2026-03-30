ce5b91779165d1d9b60a8d771ba75d1647887561
leofelix077 <leonardoaalf077@hotmail.com>
2026-03-30 11:09:55 -0300

    remove orphan translation keys and debug props on test

--- extension/src/popup/views/__tests__/SignTransaction.test.tsx
+++ extension/src/popup/views/__tests__/SignTransaction.test.tsx
@@ -110,22 +110,6 @@ jest.mock("helpers/metrics", () => ({
   emitMetric: jest.fn(),
   initAmplitude: jest.fn(),
   metricsMiddleware: jest.fn(),
-  getAnalyticsDebugInfo: jest.fn(() => ({
-    hasInitialized: false,
-    hasAmplitudeKey: false,
-    userId: null,
-    isSendingToAmplitude: false,
-  })),
-  getDebugInfoSnapshot: jest.fn(() => ({
-    hasInitialized: false,
-    hasAmplitudeKey: false,
-    userId: null,
-    isSendingToAmplitude: false,
-  })),
-  subscribeToDebugInfo: jest.fn(() => () => {}),
-  getRecentEvents: jest.fn(() => []),
-  clearRecentEvents: jest.fn(),
-  subscribeToDebugEvents: jest.fn(() => () => {}),
 }));
 
 const defaultSettingsState = {
