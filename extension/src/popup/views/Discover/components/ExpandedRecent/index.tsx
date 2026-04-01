a1f53b33abe25b34ba4fed84809f7805bb947c3e
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2026-04-01 12:07:40 -0700

    allow dismissing popover menu by tapping on the outside

--- extension/src/popup/views/Discover/components/ExpandedRecent/index.tsx
+++ extension/src/popup/views/Discover/components/ExpandedRecent/index.tsx
@@ -5,6 +5,11 @@ import { useTranslation } from "react-i18next";
 import { DiscoverData } from "@shared/api/types";
 import { SubviewHeader } from "popup/components/SubviewHeader";
 import { View } from "popup/basics/layout/View";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "popup/basics/shadcn/Popover";
 import { ProtocolRow } from "../ProtocolRow";
 
 import "./styles.scss";
@@ -27,7 +32,7 @@ export const ExpandedRecent = ({
   onClearRecent,
 }: ExpandedRecentProps) => {
   const { t } = useTranslation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   return (
     <View>
@@ -35,32 +40,35 @@ export const ExpandedRecent = ({
         title={t("Recent")}
         customBackAction={onBack}
         rightButton={
-          <div className="ExpandedRecent__menu">
-            <button
-              className="ExpandedRecent__menu-trigger"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              data-testid="expanded-recent-menu"
+          <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+            <PopoverTrigger asChild onClick={() => setIsPopoverOpen(true)}>
+              <button
+                className="ExpandedRecent__menu-trigger"
+                data-testid="expanded-recent-menu"
+              >
+                <Icon.DotsHorizontal />
+              </button>
+            </PopoverTrigger>
+            <PopoverContent
+              align="end"
+              sideOffset={8}
+              className="ExpandedRecent__dropdown"
             >
-              <Icon.DotsHorizontal />
-            </button>
-            {isMenuOpen && (
-              <div className="ExpandedRecent__dropdown">
-                <div
-                  className="ExpandedRecent__dropdown-item"
-                  onClick={() => {
-                    setIsMenuOpen(false);
-                    onClearRecent();
-                  }}
-                  data-testid="clear-recents-button"
-                >
-                  <Icon.Trash01 />
-                  <Text as="div" size="sm" weight="medium">
-                    {t("Clear recents")}
-                  </Text>
-                </div>
+              <div
+                className="ExpandedRecent__dropdown-item"
+                onClick={() => {
+                  setIsPopoverOpen(false);
+                  onClearRecent();
+                }}
+                data-testid="clear-recents-button"
+              >
+                <Icon.Trash01 />
+                <Text as="div" size="sm" weight="medium">
+                  {t("Clear recents")}
+                </Text>
               </div>
-            )}
-          </div>
+            </PopoverContent>
+          </Popover>
         }
       />
       <View.Content hasNoTopPadding>
